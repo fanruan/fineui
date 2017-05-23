9a94cbcb5654bb9d39a5282596ed0daa681a0c2b
guy <guy@finereport.com>
2017-05-24 01:34:47 +0800

    add

--- docs/demo.js
+++ docs/demo.js
@@ -1739,6 +1739,47 @@ BI.shortcut("demo.calendar", Demo.Func);Demo.Func = BI.inherit(BI.Widget, {
     },
 
     render: function () {
+        var editor = BI.createWidget({
+            type: "bi.text_editor",
+            width: 200,
+            height: 30,
+            value: "这是复制的内容"
+        });
+        var clipboard = BI.createWidget({
+            type: 'bi.clipboard',
+            width: 100,
+            height: 100,
+            cls: 'layout-bg1',
+            text: function () {
+                return editor.getValue();
+            },
+
+            afterCopy: function () {
+                BI.Msg.toast(editor.getValue());
+            }
+        });
+
+        BI.createWidget({
+            type: "bi.absolute",
+            element: this,
+            items: [{
+                el: editor,
+                left: 100,
+                top: 50,
+            }, {
+                el: clipboard,
+                left: 100,
+                top: 100
+            }]
+        })
+    }
+});
+BI.shortcut("demo.clipboard", Demo.Func);Demo.Func = BI.inherit(BI.Widget, {
+    props: {
+        baseCls: "demo-func"
+    },
+
+    render: function () {
         return {
             type: "bi.absolute",
             items: [{
@@ -1788,47 +1829,6 @@ BI.shortcut("demo.color_chooser", Demo.Func);Demo.Func = BI.inherit(BI.Widget, {
     }
 });
 BI.shortcut("demo.complex_canvas", Demo.Func);Demo.Func = BI.inherit(BI.Widget, {
-    props: {
-        baseCls: "demo-func"
-    },
-
-    render: function () {
-        var editor = BI.createWidget({
-            type: "bi.text_editor",
-            width: 200,
-            height: 30,
-            value: "这是复制的内容"
-        });
-        var clipboard = BI.createWidget({
-            type: 'bi.clipboard',
-            width: 100,
-            height: 100,
-            cls: 'layout-bg1',
-            text: function () {
-                return editor.getValue();
-            },
-
-            afterCopy: function () {
-                BI.Msg.toast(editor.getValue());
-            }
-        });
-
-        BI.createWidget({
-            type: "bi.absolute",
-            element: this,
-            items: [{
-                el: editor,
-                left: 100,
-                top: 50,
-            }, {
-                el: clipboard,
-                left: 100,
-                top: 100
-            }]
-        })
-    }
-});
-BI.shortcut("demo.clipboard", Demo.Func);Demo.Func = BI.inherit(BI.Widget, {
     props: {
         baseCls: "demo-func"
     },
