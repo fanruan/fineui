b14601a11c5428c57850037880d435722f94eb2d
guy <guy@finereport.com>
2017-06-28 15:37:44 +0800

    add

--- demo/version.js
+++ demo/version.js
@@ -2,10 +2,11 @@ Demo = {
     version: 1.0
 };
 
-BI.i18n = {
-    "BI-Basic_OK": "确定"
-};
 
 BI.servletURL = "";
 BI.resourceURL = "resource/";
-BI.i18n = {};
+BI.i18n = {
+    "BI-Basic_OK": "确定",
+    "BI-Basic_Sure": "确定",
+    "BI-Basic_Clears": "清空"
+};
