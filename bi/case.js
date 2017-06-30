462a3c546f62c1edfb7799cf9713ab3d71c5bace
guy <guy@finereport.com>
2017-06-30 17:54:45 +0800

    add

--- bi/case.js
+++ bi/case.js
@@ -3983,9 +3983,11 @@ BI.ColorChooserTrigger = BI.inherit(BI.Trigger, {
     setValue: function (color) {
         BI.ColorChooserTrigger.superclass.setValue.apply(this, arguments);
         if (color === "") {
-            this.colorContainer.element.css("background-color", "").addClass("none-color")
+            this.colorContainer.element.css("background-color", "").removeClass("trans-color-background").addClass("auto-color-background");
+        } else if (color === "transparent") {
+            this.colorContainer.element.css("background-color", "").removeClass("auto-color-background").addClass("trans-color-background")
         } else {
-            this.colorContainer.element.css({"background-color": color}).removeClass("none-color");
+            this.colorContainer.element.css({"background-color": color}).removeClass("auto-color-background").removeClass("trans-color-background");
         }
     }
 });
