9b79fac8ca7dc9b5da99ba7622fa32412e68ce6f
windy <1374721899@qq.com>
2017-07-05 09:54:48 +0800

    给downlistcombo开一些接口

--- src/widget/downlist/combo.downlist.js
+++ src/widget/downlist/combo.downlist.js
@@ -9,6 +9,7 @@ BI.DownListCombo = BI.inherit(BI.Widget, {
             height: 25,
             items: [],
             adjustLength: 0,
+            direction: "bottom",
             el: {}
         })
     },
@@ -38,6 +39,7 @@ BI.DownListCombo = BI.inherit(BI.Widget, {
             type: 'bi.combo',
             isNeedAdjustWidth: false,
             adjustLength: o.adjustLength,
+            direction: o.direction,
             el: BI.createWidget(o.el, {
                 type: "bi.icon_trigger",
                 extraCls: o.iconCls ? o.iconCls : "pull-down-font",
@@ -58,6 +60,10 @@ BI.DownListCombo = BI.inherit(BI.Widget, {
 
     hideView: function () {
         this.downlistcombo.hideView();
+    },
+
+    showView: function () {
+        this.downlistcombo.showView();
     },
 
     populate: function (items) {
