53caebc6cc9b28e628cba26248c79cd08a83901f
guy <guy@finereport.com>
2017-08-01 15:47:22 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,7 +127,6 @@ module.exports = function (grunt) {
                     'src/core/shortcut.js',
                     'src/core/plugin.js',
                     'src/core/controller.js',
-                    'src/core/load.js',
                     'src/core/proto/**/*.js',
                     'src/core/utils/**/*.js',
                     'src/core/behavior/behavior.js',
