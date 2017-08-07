de524bcd02405f1400ffa9b7c2299245a62d9ce4
Urthur <urthur@fanruan.com>
2017-08-07 09:46:24 +0800

    BI-7322 日期 时分秒基本控件  日期格式打印方式修改

--- demo/js/widget/demo.datetime.js
+++ demo/js/widget/demo.datetime.js
@@ -13,11 +13,18 @@ Demo.CustomDateTime = BI.inherit(BI.Widget, {
                     type: "bi.custom_date_time_combo",
                     ref: function (_ref) {
                         self.customDateTime =  _ref;
+                        var value, date, dateStr;
                         self.customDateTime.on(BI.CustomDateTimeCombo.EVENT_CONFIRM, function () {
-                            BI.Msg.alert("日期", this.getValue().text);
+                            value = this.getValue();
+                            date = new Date(value.year,value.month,value.day,value.hour,value.minute,value.second);
+                            dateStr = date.print("%Y-%X-%d %H:%M:%S");
+                            BI.Msg.alert("日期", dateStr);
                         });
                         self.customDateTime.on(BI.CustomDateTimeCombo.EVENT_CANCEL, function () {
-                            BI.Msg.alert("日期", this.getValue().text);
+                            value = this.getValue();
+                            date = new Date(value.year,value.month,value.day,value.hour,value.minute,value.second);
+                            dateStr = date.print("%Y-%X-%d %H:%M:%S");
+                            BI.Msg.alert("日期", dateStr);
                         });
                     }
                 },
--- src/widget/datetime/datetime.trigger.js
+++ src/widget/datetime/datetime.trigger.js
@@ -4,15 +4,16 @@
 BI.DateTimeTrigger = BI.inherit(BI.Trigger, {
     _const: {
         hgap: 4,
-        vgap: 2,
         triggerWidth: 30
     },
 
     _defaultConfig: function () {
         return BI.extend(BI.DateTimeTrigger.superclass._defaultConfig.apply(this, arguments), {
             extraCls: "bi-date-time-trigger",
-            height: 25,
-            width: 180
+            min: '1900-01-01', //最小日期
+            max: '2099-12-31', //最大日期
+            height: 24,
+            width: 200
         });
     },
     _init: function () {
@@ -20,19 +21,18 @@ BI.DateTimeTrigger = BI.inherit(BI.Trigger, {
         var self = this, o = this.options, c = this._const;
         this.text = BI.createWidget({
             type: "bi.label",
-            cls: "bi-border",
             textAlign: "left",
             height: o.height,
             width: o.width,
-            hgap: c.hgap,
-            vgap: c.vgap
+            hgap: c.hgap
         });
+
         BI.createWidget({
             type: "bi.htape",
             element: this,
             items: [{
                 el: BI.createWidget(),
-                width: 30
+                width: c.triggerWidth
             }, {
                 el: this.text
             }]
@@ -45,24 +45,18 @@ BI.DateTimeTrigger = BI.inherit(BI.Trigger, {
 
     setValue: function (v) {
         var self = this;
-        if (BI.isNotNull(v)) {
-            var value = v.value, dateStr;
-            if(BI.isNull(value)){
-                value = new Date();
-                dateStr = value.getFullYear() + "-" + self._printTime(value.getMonth() + 1) + "-" + self._printTime(value.getDate())
-                    + " " + self._printTime(value.getHours()) + ":" + self._printTime(value.getMinutes()) + ":" + self._printTime(value.getSeconds());
-            } else {
-                dateStr = value.year + "-" + self._printTime(value.month + 1) + "-" + self._printTime(value.day)
-                    + " " + self._printTime(value.hour) + ":" + self._printTime(value.minute) + ":" + self._printTime(value.second);
-            }
-            this.text.setText(dateStr);
-            this.text.setTitle(dateStr);
+        var value = v, dateStr;
+        if(BI.isNull(value)){
+            value = new Date();
+            dateStr = value.print("%Y-%X-%d %H:%M:%S");
+        } else {
+            var date = new Date(value.year,value.month,value.day,value.hour,value.minute,value.second);
+            dateStr = date.print("%Y-%X-%d %H:%M:%S");
+
         }
-    },
-    getValue: function () {
-        return this.text.getText();
+        this.text.setText(dateStr);
+        this.text.setTitle(dateStr);
     }
 
 });
-BI.DateTrigger.EVENT_TRIGGER_CLICK = "EVENT_TRIGGER_CLICK";
 BI.shortcut("bi.date_time_trigger", BI.DateTimeTrigger);
