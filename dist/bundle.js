0cc24fcea1342cf5c93d9cca9bbeb5fa3a47ee64
windy <1374721899@qq.com>
2017-09-06 12:43:37 +0800

    控件抽象

--- dist/bundle.js
+++ dist/bundle.js
@@ -28,7 +28,8 @@ BI.i18n = {
     "BI-Load_More": "加载更多",
     "BI-Select_All": "全选",
     "BI-Basic_Auto": "自动",
-    "BI-No_More_Data": "无更多数据"
+    "BI-No_More_Data": "无更多数据",
+    "BI-No_Selected_Value": "没有可选项"
 };/*!
  * jQuery JavaScript Library v1.9.1
  * http://jquery.com/
@@ -82542,35 +82543,36 @@ BI.extend(BI.FileManagerNav, {
 });
 BI.FileManagerNav.EVENT_CHANGE = "FileManagerNav.EVENT_CHANGE";
 BI.shortcut("bi.file_manager_nav", BI.FileManagerNav);/**
- * Created by User on 2017/9/5.
+ * Created by windy on 2017/3/13.
+ * 数值微调器
  */
-BI.CustomFineTuningNumberEditor = BI.inherit(BI.Widget, {
+BI.FineTuningNumberEditor = BI.inherit(BI.Widget, {
     _defaultConfig: function () {
-        return BI.extend(BI.CustomFineTuningNumberEditor.superclass._defaultConfig.apply(this, arguments), {
+        return BI.extend(BI.FineTuningNumberEditor.superclass._defaultConfig.apply(this, arguments), {
             baseCls: "bi-fine-tuning-number-editor bi-border",
+            validationChecker: function () {return true;},
+            valueFormatter: function (v) {return v;},
             value: 0,
-            validationChecker: function () {
-                return true;
-            },
             errorText: ""
         })
     },
 
     _init: function () {
-        BI.CustomFineTuningNumberEditor.superclass._init.apply(this, arguments);
+        BI.FineTuningNumberEditor.superclass._init.apply(this, arguments);
         var self = this, o = this.options;
         this.editor = BI.createWidget({
             type: "bi.sign_editor",
             height: o.height,
-            value: o.value,
+            value: o.valueFormatter(o.value),
             validationChecker: o.validationChecker,
             errorText: o.errorText
         });
         this.editor.on(BI.TextEditor.EVENT_CHANGE, function () {
-            self.fireEvent(BI.CustomFineTuningNumberEditor.EVENT_CHANGE);
+            o.value = this.getValue();
+            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CHANGE);
         });
         this.editor.on(BI.TextEditor.EVENT_CONFIRM, function(){
-            self.fireEvent(BI.CustomFineTuningNumberEditor.EVENT_CONFIRM);
+            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CONFIRM);
         });
         this.topBtn = BI.createWidget({
             type: "bi.icon_button",
@@ -82579,8 +82581,8 @@ BI.CustomFineTuningNumberEditor = BI.inherit(BI.Widget, {
         });
         this.topBtn.on(BI.IconButton.EVENT_CHANGE, function(){
             self._finetuning(1);
-            self.fireEvent(BI.CustomFineTuningNumberEditor.EVENT_CHANGE);
-            self.fireEvent(BI.CustomFineTuningNumberEditor.EVENT_CONFIRM);
+            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CHANGE);
+            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CONFIRM);
         });
         this.bottomBtn = BI.createWidget({
             type: "bi.icon_button",
@@ -82589,8 +82591,8 @@ BI.CustomFineTuningNumberEditor = BI.inherit(BI.Widget, {
         });
         this.bottomBtn.on(BI.IconButton.EVENT_CHANGE, function(){
             self._finetuning(-1);
-            self.fireEvent(BI.CustomFineTuningNumberEditor.EVENT_CHANGE);
-            self.fireEvent(BI.CustomFineTuningNumberEditor.EVENT_CONFIRM);
+            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CHANGE);
+            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CONFIRM);
         });
         BI.createWidget({
             type: "bi.htape",
@@ -82617,8 +82619,8 @@ BI.CustomFineTuningNumberEditor = BI.inherit(BI.Widget, {
 
     //微调
     _finetuning: function(add){
-        var v = BI.parseFloat(this.editor.getValue());
-        this.editor.setValue(v.add(add));
+        var v = BI.parseFloat(this.getValue());
+        this.setValue(v.add(add));
     },
 
     setUpEnable: function (v) {
@@ -82630,113 +82632,18 @@ BI.CustomFineTuningNumberEditor = BI.inherit(BI.Widget, {
     },
 
     getValue: function () {
-        return this.editor.getValue();
+        return this.options.value;
     },
 
     setValue: function (v) {
-        this.editor.setValue(v);
-    }
-
-});
-BI.CustomFineTuningNumberEditor.EVENT_CHANGE = "EVENT_CHANGE";
-BI.CustomFineTuningNumberEditor.EVENT_CONFIRM = "EVENT_CONFIRM";
-BI.shortcut("bi.custom_fine_tuning_number_editor", BI.CustomFineTuningNumberEditor);/**
- * Created by windy on 2017/3/13.
- * 数值微调器
- */
-BI.FineTuningNumberEditor = BI.inherit(BI.Widget, {
-    _defaultConfig: function () {
-        return BI.extend(BI.FineTuningNumberEditor.superclass._defaultConfig.apply(this, arguments), {
-            baseCls: "bi-fine-tuning-number-editor bi-border",
-            value: -1
-        })
-    },
-
-    _init: function () {
-        BI.FineTuningNumberEditor.superclass._init.apply(this, arguments);
-        var self = this, o = this.options;
-        this.editor = BI.createWidget({
-            type: "bi.sign_editor",
-            height: o.height,
-            value: this._alertInEditorValue(o.value),
-            errorText: BI.i18nText("BI-Please_Input_Natural_Number"),
-            validationChecker: function(v){
-                return BI.isNaturalNumber(v) || self._alertOutEditorValue(v) === -1;
-            }
-        });
-        this.editor.on(BI.TextEditor.EVENT_CONFIRM, function(){
-            self._finetuning(0);
-            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CONFIRM);
-        });
-        this.topBtn = BI.createWidget({
-            type: "bi.icon_button",
-            trigger: "lclick,",
-            cls: "column-pre-page-h-font top-button bi-border-left bi-border-bottom"
-        });
-        this.topBtn.on(BI.IconButton.EVENT_CHANGE, function(){
-            self._finetuning(1);
-            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CONFIRM);
-        });
-        this.bottomBtn = BI.createWidget({
-            type: "bi.icon_button",
-            trigger: "lclick,",
-            cls: "column-next-page-h-font bottom-button bi-border-left bi-border-top"
-        });
-        this.bottomBtn.on(BI.IconButton.EVENT_CHANGE, function(){
-            self._finetuning(-1);
-            self.fireEvent(BI.FineTuningNumberEditor.EVENT_CONFIRM);
-        });
-        this._finetuning(0);
-        BI.createWidget({
-            type: "bi.htape",
-            element: this,
-            items: [this.editor, {
-                el: {
-                    type: "bi.grid",
-                    columns: 1,
-                    rows: 2,
-                    items: [{
-                        column: 0,
-                        row: 0,
-                        el: this.topBtn
-                    }, {
-                        column: 0,
-                        row: 1,
-                        el: this.bottomBtn
-                    }]
-                },
-                width: 23
-            }]
-        });
-    },
-
-    _alertOutEditorValue: function(v){
-        return v === BI.i18nText("BI-Basic_Auto") ? -1 : v;
-    },
-
-    _alertInEditorValue: function(v){
-        return BI.parseInt(v) === -1 ? BI.i18nText("BI-Basic_Auto") : v;
-    },
-
-    //微调
-    _finetuning: function(add){
-        var v = BI.parseInt(this._alertOutEditorValue(this.editor.getValue()));
-        this.editor.setValue(this._alertInEditorValue(v + add));
-        this.bottomBtn.setEnable((v + add) > -1);
-    },
-
-    getValue: function () {
-        var v = this.editor.getValue();
-        return this._alertOutEditorValue(v);
-    },
-
-    setValue: function (v) {
-        this.editor.setValue(this._alertInEditorValue(v));
-        this._finetuning(0);
+        var o = this.options;
+        o.value = v;
+        this.editor.setValue(o.valueFormatter(v));
     }
 
 });
 BI.FineTuningNumberEditor.EVENT_CONFIRM = "EVENT_CONFIRM";
+BI.FineTuningNumberEditor.EVENT_CHANGE = "EVENT_CHANGE";
 BI.shortcut("bi.fine_tuning_number_editor", BI.FineTuningNumberEditor);/**
  * 交互行为布局
  *
