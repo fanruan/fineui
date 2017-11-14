f8c12dbfcc4af26b80ac615cb8cbef644676d73e
windy <1374721899@qq.com>
2017-11-14 09:15:37 +0800

    日期tab输入框宽度60

--- dist/widget.js
+++ dist/widget.js
@@ -7952,7 +7952,7 @@ BI.MultiDateSegment = BI.inherit(BI.Single, {
         itemHeight: 24,
         maxGap: 15,
         minGap: 10,
-        textWidth: 30,
+        textWidth: 60,
         defaultEditorValue: "1"
     },
 
@@ -7960,7 +7960,6 @@ BI.MultiDateSegment = BI.inherit(BI.Single, {
         return $.extend(BI.MultiDateSegment.superclass._defaultConfig.apply(this, arguments), {
             baseCls: 'bi-multidate-segment',
             text: "",
-            width: 130,
             height: 30,
             isEditorExist: true,
             selected: false,
@@ -7979,13 +7978,13 @@ BI.MultiDateSegment = BI.inherit(BI.Single, {
             self.fireEvent(BI.Controller.EVENT_CHANGE, arguments);
         });
         this.textEditor = BI.createWidget({
-            type: 'bi.sign_editor',
+            type: 'bi.text_editor',
             value: this.constants.defaultEditorValue,
             title: function () {
                 return self.textEditor.getValue();
             },
             tipType: "success",
-            cls: 'bi-multidate-editor bi-border',
+            cls: 'bi-multidate-editor',
             width: this.constants.textWidth,
             height: this.constants.itemHeight
         });
