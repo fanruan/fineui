f0c4d3dfb4b2b4ca1e2e049d04701743ec2d889d
guy <guy@finereport.com>
2017-12-15 09:31:17 +0800

    update

--- src/base/combination/loader.js
+++ src/base/combination/loader.js
@@ -109,11 +109,9 @@ BI.Loader = BI.inherit(BI.Widget, {
         o.isDefaultInit && BI.isEmpty(o.items) && BI.nextTick(BI.bind(function () {
             o.isDefaultInit && BI.isEmpty(o.items) && this.populate();
         }, this));
-        /*
         if (BI.isNotEmptyArray(o.items)) {
             this.populate(o.items);
         }
-        */
     },
 
     hasPrev: function () {
