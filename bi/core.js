1d3954266ec699b0d23a8be72f4cd39e6eabdeb5
guy <guy@finereport.com>
2017-12-18 11:13:01 +0800

    update命名

--- bi/core.js
+++ bi/core.js
@@ -12913,8 +12913,8 @@ BI.Widget = BI.inherit(BI.OB, {
 
     // 根据配置属性生成widget
     var createWidget = function (config) {
-        if (config['classType']) {
-            return new (new Function('return ' + config['classType'] + ';')())(config);
+        if (config["classType"]) {
+            return new (new Function("return " + config["classType"] + ";")())(config);
         }
 
         var cls = kv[config.type];
@@ -12935,18 +12935,18 @@ BI.Widget = BI.inherit(BI.OB, {
         if (item && (item.type || options.type)) {
             el = BI.extend({}, options, item);
             w = BI.Plugin.getWidget(el.type, el);
-            return BI.Plugin.getObject(el.type, w.type === el.type ? createWidget(w) : BI.createWidget(BI.extend({}, item, {type: w.type}, options)));
+            return w.type === el.type ? BI.Plugin.getObject(el.type, createWidget(w)) : BI.createWidget(BI.extend({}, item, {type: w.type}, options));
         }
         if (item && item.el && (item.el.type || options.type)) {
             el = BI.extend({}, options, item.el);
             w = BI.Plugin.getWidget(el.type, el);
-            return BI.Plugin.getObject(el.type, w.type === el.type ? createWidget(w) : BI.createWidget(BI.extend({}, item, {type: w.type}, options)));
+            return w.type === el.type ? BI.Plugin.getObject(el.type, createWidget(w)) : BI.createWidget(BI.extend({}, item, {type: w.type}, options));
         }
         if (item && BI.isWidget(item.el)) {
             return item.el;
         }
-        throw new Error('无法根据item创建组件');
-    }
+        throw new Error("无法根据item创建组件");
+    };
 
 })();(function (window, undefined) {
     function aspect(type) {
