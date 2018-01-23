80157863239ee0c38198cc45eff70f247233d4cd
windy <1374721899@qq.com>
2018-01-23 12:21:33 +0800

    multi_layer_level_tree的修改

--- dist/bundle.js
+++ dist/bundle.js
@@ -84469,7 +84469,7 @@ BI.MultiLayerSelectLevelTree = BI.inherit(BI.Widget, {
     },
 
     getValue: function () {
-        return this.tree.getValue();
+        return BI.uniq(this.tree.getValue());
     },
 
     getAllLeaves: function () {
@@ -85019,7 +85019,7 @@ BI.MultiLayerSingleLevelTree = BI.inherit(BI.Widget, {
     },
 
     getValue: function () {
-        return this.tree.getValue();
+        return BI.uniq(this.tree.getValue());
     },
 
     getAllLeaves: function () {
