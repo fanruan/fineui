54002449b0a80487c79082264da3edab68c3d2b1
windy <1374721899@qq.com>
2018-01-23 15:47:56 +0800

    去setValue

--- dist/fineui.js
+++ dist/fineui.js
@@ -91442,8 +91442,6 @@ BI.NumberInterval = BI.inherit(BI.Single, {
         self._setComboValueChangedEvent(self.smallCombo);
         self._setEditorValueChangedEvent(self.bigEditor);
         self._setEditorValueChangedEvent(self.smallEditor);
-        
-        this.setValue(o.value);
     },
 
     _checkValidation: function () {
