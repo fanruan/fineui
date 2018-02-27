36bf90338247227e37a996cd51629ac1770c876f
guy <guy@finereport.com>
2018-02-27 17:27:07 +0800

    update

--- src/core/lodash.js
+++ src/core/lodash.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lodash (Custom Build) <https://lodash.com/>
- * Build: `lodash core plus="debounce,throttle,get,findIndex,findLastIndex,findKey,findLastKey,isArrayLike,invert,invertBy,uniq,uniqBy,omit,omitBy,zip,unzip,rest,range"`
+ * Build: `lodash core plus="debounce,throttle,get,findIndex,findLastIndex,findKey,findLastKey,isArrayLike,invert,invertBy,uniq,uniqBy,omit,omitBy,zip,unzip,rest,range,random"`
  * Copyright JS Foundation and other contributors <https://js.foundation/>
  * Released under MIT license <https://lodash.com/license>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -233,7 +233,8 @@
   };
 
   /** Built-in method references without a dependency on `root`. */
-  var freeParseInt = parseInt;
+  var freeParseFloat = parseFloat,
+      freeParseInt = parseInt;
 
   /** Detect free variable `global` from Node.js. */
   var freeGlobal = typeof global == 'object' && global && global.Object === Object && global;
@@ -982,6 +983,7 @@
 
   /* Built-in method references for those with the same name as other `lodash` methods. */
   var nativeCeil = Math.ceil,
+      nativeFloor = Math.floor,
       nativeGetSymbols = Object.getOwnPropertySymbols,
       nativeIsBuffer = Buffer ? Buffer.isBuffer : undefined,
       nativeIsFinite = root.isFinite,
@@ -989,6 +991,7 @@
       nativeMax = Math.max,
       nativeMin = Math.min,
       nativeNow = Date.now,
+      nativeRandom = Math.random,
       nativeReverse = arrayProto.reverse;
 
   /* Built-in method references that are verified to be native. */
@@ -2783,6 +2786,19 @@
     return function(object) {
       return baseGet(object, path);
     };
+  }
+
+  /**
+   * The base implementation of `_.random` without support for returning
+   * floating-point numbers.
+   *
+   * @private
+   * @param {number} lower The lower bound.
+   * @param {number} upper The upper bound.
+   * @returns {number} Returns the random number.
+   */
+  function baseRandom(lower, upper) {
+    return lower + nativeFloor(nativeRandom() * (upper - lower + 1));
   }
 
   /**
@@ -8258,6 +8274,78 @@
   /*------------------------------------------------------------------------*/
 
   /**
+   * Produces a random number between the inclusive `lower` and `upper` bounds.
+   * If only one argument is provided a number between `0` and the given number
+   * is returned. If `floating` is `true`, or either `lower` or `upper` are
+   * floats, a floating-point number is returned instead of an integer.
+   *
+   * **Note:** JavaScript follows the IEEE-754 standard for resolving
+   * floating-point values which can produce unexpected results.
+   *
+   * @static
+   * @memberOf _
+   * @since 0.7.0
+   * @category Number
+   * @param {number} [lower=0] The lower bound.
+   * @param {number} [upper=1] The upper bound.
+   * @param {boolean} [floating] Specify returning a floating-point number.
+   * @returns {number} Returns the random number.
+   * @example
+   *
+   * _.random(0, 5);
+   * // => an integer between 0 and 5
+   *
+   * _.random(5);
+   * // => also an integer between 0 and 5
+   *
+   * _.random(5, true);
+   * // => a floating-point number between 0 and 5
+   *
+   * _.random(1.2, 5.2);
+   * // => a floating-point number between 1.2 and 5.2
+   */
+  function random(lower, upper, floating) {
+    if (floating && typeof floating != 'boolean' && isIterateeCall(lower, upper, floating)) {
+      upper = floating = undefined;
+    }
+    if (floating === undefined) {
+      if (typeof upper == 'boolean') {
+        floating = upper;
+        upper = undefined;
+      }
+      else if (typeof lower == 'boolean') {
+        floating = lower;
+        lower = undefined;
+      }
+    }
+    if (lower === undefined && upper === undefined) {
+      lower = 0;
+      upper = 1;
+    }
+    else {
+      lower = toFinite(lower);
+      if (upper === undefined) {
+        upper = lower;
+        lower = 0;
+      } else {
+        upper = toFinite(upper);
+      }
+    }
+    if (lower > upper) {
+      var temp = lower;
+      lower = upper;
+      upper = temp;
+    }
+    if (floating || lower % 1 || upper % 1) {
+      var rand = nativeRandom();
+      return nativeMin(lower + (rand * (upper - lower + freeParseFloat('1e-' + ((rand + '').length - 1)))), upper);
+    }
+    return baseRandom(lower, upper);
+  }
+
+  /*------------------------------------------------------------------------*/
+
+  /**
    * Converts the characters "&", "<", ">", '"', and "'" in `string` to their
    * corresponding HTML entities.
    *
@@ -8792,6 +8880,7 @@
   lodash.min = min;
   lodash.noConflict = noConflict;
   lodash.noop = noop;
+  lodash.random = random;
   lodash.reduce = reduce;
   lodash.result = result;
   lodash.size = size;
