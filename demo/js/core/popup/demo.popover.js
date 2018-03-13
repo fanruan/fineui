48cce58cd997c6b308089e353e3f1abf4460e579
Young <young@finereport.com>
2018-03-13 09:18:22 +0800

    BI-16736 popover优化——加入size属性&fineui demo中加入简单的搜索

--- demo/js/core/popup/demo.popover.js
+++ demo/js/core/popup/demo.popover.js
@@ -12,12 +12,13 @@ Demo.Func = BI.inherit(BI.Widget, {
             vgap: 10,
             items: [{
                 type: "bi.text_button",
-                text: "点击弹出Popover",
+                text: "点击弹出Popover(normal size)",
                 height: 30,
                 handler: function () {
                     BI.Popovers.remove(id);
                     BI.Popovers.create(id, {
                         type: "bi.bar_popover",
+                        size: "big",
                         header: {
                             type: "bi.label",
                             text: "这个是header"
@@ -26,10 +27,67 @@ Demo.Func = BI.inherit(BI.Widget, {
                             type: "bi.label",
                             text: "这个是body"
                         }
-                        // footer: {
-                        //     type: "bi.label",
-                        //     text: "这个是footer"
-                        // }
+                    }).open(id);
+                }
+            }, {
+                type: "bi.text_button",
+                text: "点击弹出Popover(small size)",
+                height: 30,
+                handler: function () {
+                    BI.Popovers.remove(id);
+                    BI.Popovers.create(id, {
+                        type: "bi.bar_popover",
+                        size: "small",
+                        header: {
+                            type: "bi.label",
+                            text: "这个是header"
+                        },
+                        body: {
+                            type: "bi.label",
+                            text: "这个是body"
+                        }
+                    }).open(id);
+                }
+            }, {
+                type: "bi.text_button",
+                text: "点击弹出Popover(big size)",
+                height: 30,
+                handler: function () {
+                    BI.Popovers.remove(id);
+                    BI.Popovers.create(id, {
+                        type: "bi.bar_popover",
+                        size: "big",
+                        header: {
+                            type: "bi.label",
+                            text: "这个是header"
+                        },
+                        body: {
+                            type: "bi.label",
+                            text: "这个是body"
+                        }
+                    }).open(id);
+                }
+            }, {
+                type: "bi.text_button",
+                text: "点击弹出Popover(custom)",
+                height: 30,
+                handler: function () {
+                    BI.Popovers.remove(id);
+                    BI.Popovers.create(id, {
+                        width: 400,
+                        height: 300,
+                        header: {
+                            type: "bi.label",
+                            text: "这个是header"
+                        },
+                        body: {
+                            type: "bi.label",
+                            text: "这个是body"
+                        },
+                        footer: {
+                            type: "bi.label",
+                            text: "这个是footer"
+                        }
                     }).open(id);
                 }
             }]
