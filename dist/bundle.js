ba26d78433ba09130bf3c78b2fc901822d85fbd4
guy <guy@finereport.com>
2018-03-27 17:31:07 +0800

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -100236,12 +100236,12 @@ BI.PreviewTable = BI.inherit(BI.Widget, {
             })
         });
         this.table.on(BI.Table.EVENT_TABLE_AFTER_INIT, function () {
-            self._adjustColumns();
             self.fireEvent(BI.Table.EVENT_TABLE_AFTER_INIT, arguments);
         });
         this.table.on(BI.Table.EVENT_TABLE_RESIZE, function () {
             self._adjustColumns();
         });
+        this._adjustColumns();
     },
 
     // 是否有自适应调节的列，即列宽为""
@@ -100380,6 +100380,7 @@ BI.PreviewTable = BI.inherit(BI.Widget, {
 
     populate: function (items, header) {
         this.table.populate(items, header);
+        this._adjustColumns();
     }
 });
 BI.PreviewTable.EVENT_CHANGE = "PreviewTable.EVENT_CHANGE";
