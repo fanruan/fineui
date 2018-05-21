c02a54db4e259ba7ecddbec749acccbef714324d
windy <1374721899@qq.com>
2018-05-21 14:05:35 +0800

    update

--- dist/demo.js
+++ dist/demo.js
@@ -13353,7 +13353,7 @@ Demo.SingleSelectCombo = BI.inherit(BI.Widget, {
     _createSingleSelectCombo: function () {
         var self = this;
         var widget = BI.createWidget({
-            type: "bi.single_select_insert_list",
+            type: "bi.single_select_insert_combo",
             itemsCreator: BI.bind(this._itemsCreator, this),
             width: 200,
             height: 600,
