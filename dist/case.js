d7acc52e9443ce70e854545dd77c593f67d84bf4
windy <1374721899@qq.com>
2018-06-13 14:42:40 +0800

    update

--- dist/case.js
+++ dist/case.js
@@ -10851,21 +10851,12 @@ BI.shortcut("bi.rich_editor_text_toolbar", BI.RichEditorTextToolbar);/**
                 // this.setContent("<br />");
             }
             this.instanceDoc = document.defaultView;
-            this.elm.element.on("mousedown", BI.bind(function (e) {
-                console.log(1);
-                this.selected(e);
-            }, this));
+            this.elm.element.on("mousedown", BI.bind(this.selected, this));
             this.elm.element.on("keyup", BI.bind(this.keyDown, this));
             // this.elm.element.on("keydown", BI.bind(this.keyDown, this));
-            this.elm.element.on("focus", BI.bind(function (e) {
-                console.log(2);
-                this.selected(e);
-            }, this));
+            this.elm.element.on("focus", BI.bind(this.selected, this));
             this.elm.element.on("blur", BI.bind(this.blur, this));
-            this.elm.element.on("keyup", BI.bind(function (e) {
-                console.log(3);
-                this.selected(e);
-            }, this));
+            this.elm.element.on("keyup", BI.bind(this.selected, this));
             this.ne.fireEvent("add");
         },
 
