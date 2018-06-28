d5ca434dc71238c5b8e13d3818b2c3591a33fb49
iapyang <yklorse@gmail.com>
2018-06-28 12:37:18 +0800

    update

--- dist/fineui.js
+++ dist/fineui.js
@@ -94494,7 +94494,7 @@ BI.Filter.FILTER_TYPE.EMPTY_CONDITION = 37;
                 }
             });
         },
-        
+
         props: {
             baseCls: "bi-filter-operation",
             expander: {},
@@ -94661,7 +94661,7 @@ BI.Filter.FILTER_TYPE.EMPTY_CONDITION = 37;
                     iconCls2: "pull-down-font",
                     text: text,
                     height: 30,
-                    width: 100
+                    width: 110
                 });
                 combos.push({
                     type: "bi.combo",
