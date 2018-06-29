281a5a187dba2843211733fc337ecf2240ea47bc
windy <1374721899@qq.com>
2018-06-29 14:02:30 +0800

    update

--- dist/fineui.js
+++ dist/fineui.js
@@ -108794,7 +108794,8 @@ BI.SliderIconButton = BI.inherit(BI.Widget, {
             items: [{
                 el: {
                     type: "bi.text_button",
-                    cls: "slider-button",
+                    forceNotSelected: true,
+                    cls: "slider-button bi-list-item-select2",
                     ref: function () {
                         self.slider = this;
                     }
