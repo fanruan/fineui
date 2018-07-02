e726be5859fd8851631f364fbc54df6e8ad890e1
fay <lfhlifenghao@gmail.com>
2018-07-02 14:53:42 +0800

    BI-25599

--- src/widget/multilayerdownlist/popup.downlist.js
+++ src/widget/multilayerdownlist/popup.downlist.js
@@ -167,7 +167,7 @@ BI.MultiLayerDownListPopup = BI.inherit(BI.Pane, {
                     }],
                     cls: "bi-down-list-spliter-container cursor-pointer",
                     lgap: 10,
-                    rgap: 10
+                    rgap: 0
                 });
                 result.push(spliter_container);
             }
