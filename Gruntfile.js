d2537470a14df896a01c28f739a4392a1b310e82
fay <lfhlifenghao@gmail.com>
2018-07-18 11:33:37 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
                 open: true,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
-                hostname: "localhost"
+                hostname: "0.0.0.0"
             },
             server: {
                 options: {
