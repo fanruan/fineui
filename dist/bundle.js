c83b3d4f33c9d7a7097c0cce91f338e407740c7a
imp <imp@fanruan.com>
2018-08-03 10:46:05 +0800

    输出级别

--- dist/bundle.js
+++ dist/bundle.js
@@ -35110,7 +35110,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 },
                 set: function reactiveSetter(newVal) {
                     var value = childOb ? childOb.model : val;
-                    if (newVal === value || (newVal !== newVal && value !== value)) {
+                    if (newVal === value || newVal !== newVal && value !== value) {
                         return;
                     }
                     val = newVal;
@@ -35304,7 +35304,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
             } catch (e) {
                 // if (this.user) {
                 // } else {
-                console.error(e);
+                // console.error(e)
                 // }
             } finally {
                 // "touch" every property so they are all tracked as
@@ -35373,13 +35373,13 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                         try {
                             this.cb.call(this.vm, value, oldValue, options);
                         } catch (e) {
-                            console.log(e);
+                            console.error(e);
                         }
                     } else {
                         try {
                             this.cb.call(this.vm, value, oldValue, options);
                         } catch (e) {
-                            console.log(e);
+                            console.error(e);
                         }
                     }
                 }
