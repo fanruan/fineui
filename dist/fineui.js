00293e60f02c17545c4b51b14af5838824b092a1
windy <1374721899@qq.com>
2018-08-22 10:46:31 +0800

    BI-30870 文本下拉单选按钮加回去

--- dist/fineui.js
+++ dist/fineui.js
@@ -75036,6 +75036,72 @@ BI.extend(BI.SingleSelectInsertCombo, {
 BI.SingleSelectInsertCombo.EVENT_CONFIRM = "EVENT_CONFIRM";
 
 BI.shortcut("bi.single_select_insert_combo", BI.SingleSelectInsertCombo);/**
+ * guy
+ * 单选框item
+ * @type {*|void|Object}
+ */
+BI.SingleSelectComboItem = BI.inherit(BI.BasicButton, {
+    _defaultConfig: function () {
+        return BI.extend(BI.SingleSelectComboItem.superclass._defaultConfig.apply(this, arguments), {
+            extraCls: "bi-single-select-radio-item",
+            logic: {
+                dynamic: false
+            },
+            height: 24
+        });
+    },
+    _init: function () {
+        BI.SingleSelectComboItem.superclass._init.apply(this, arguments);
+        var self = this, o = this.options;
+        this.radio = BI.createWidget({
+            type: "bi.radio"
+        });
+        this.text = BI.createWidget({
+            type: "bi.label",
+            cls: "list-item-text",
+            textAlign: "left",
+            whiteSpace: "nowrap",
+            textHeight: o.height,
+            height: o.height,
+            hgap: o.hgap,
+            text: o.text,
+            keyword: o.keyword,
+            value: o.value,
+            py: o.py
+        });
+
+        BI.createWidget(BI.extend({
+            element: this
+        }, BI.LogicFactory.createLogic("horizontal", BI.extend(o.logic, {
+            items: BI.LogicFactory.createLogicItemsByDirection("left", {
+                type: "bi.center_adapt",
+                items: [this.radio],
+                width: 26
+            }, this.text)
+        }))));
+    },
+
+    doRedMark: function () {
+        this.text.doRedMark.apply(this.text, arguments);
+    },
+
+    unRedMark: function () {
+        this.text.unRedMark.apply(this.text, arguments);
+    },
+
+    doClick: function () {
+        BI.SingleSelectComboItem.superclass.doClick.apply(this, arguments);
+        this.radio.setSelected(this.isSelected());
+    },
+
+    setSelected: function (v) {
+        BI.SingleSelectComboItem.superclass.setSelected.apply(this, arguments);
+        this.radio.setSelected(v);
+
+    }
+});
+
+BI.shortcut("bi.single_select_combo.item", BI.SingleSelectComboItem);/**
  * 选择列表
  *
  * Created by GUY on 2015/11/1.
@@ -75273,7 +75339,7 @@ BI.SingleSelectLoader = BI.inherit(BI.Widget, {
 
     _createItems: function (items) {
         return BI.createItems(items, {
-            type: "bi.single_select_item",
+            type: "bi.single_select_combo.item",
             logic: this.options.logic,
             cls: "bi-list-item-active",
             height: 24,
