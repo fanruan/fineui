d3793781b79334041eec99b3bec538d099575550
windy <1374721899@qq.com>
2018-08-23 10:08:57 +0800

    无JIRA任务 边框加一下

--- dist/bundle.js
+++ dist/bundle.js
@@ -50923,6 +50923,7 @@ BI.ColorChooserPopup = BI.inherit(BI.Widget, {
 
         this.storeColors = BI.createWidget({
             type: "bi.color_picker",
+            cls: "bi-border-bottom bi-border-right",
             items: [[{
                 value: "",
                 disabled: true
