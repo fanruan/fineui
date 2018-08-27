a57e925aca7868b1200290c34b7a209939373ed9
guy <guy@finereport.com>
2018-08-27 15:05:07 +0800

    update

--- dist/core.js
+++ dist/core.js
@@ -16315,53 +16315,6 @@ BI.RedMarkBehavior = BI.inherit(BI.Behavior, {
             }
         });
     }
-});// 工程配置
-BI.prepares.push(function () {
-    // 注册布局
-    var isSupportFlex = BI.isSupportCss3("flex");
-    BI.Plugin.registerWidget("bi.horizontal", function (ob) {
-        if (!BI.isIE() && isSupportFlex) {
-            return BI.extend(ob, {type: "bi.flex_horizontal"});
-        }
-        return ob;
-
-    });
-    BI.Plugin.registerWidget("bi.center_adapt", function (ob) {
-        if (!BI.isIE() && isSupportFlex && ob.items && ob.items.length <= 1) {
-            // 有滚动条的情况下需要用到flex_wrapper_center布局
-            if (ob.scrollable === true || ob.scrollx === true || ob.scrolly === true) {
-                // 不是IE用flex_wrapper_center布局
-                return BI.extend(ob, {type: "bi.flex_wrapper_center"});
-            }
-            return BI.extend(ob, {type: "bi.flex_center"});
-        }
-        return ob;
-
-    });
-    BI.Plugin.registerWidget("bi.vertical_adapt", function (ob) {
-        if (!BI.isIE() && isSupportFlex) {
-            // 有滚动条的情况下需要用到flex_wrapper_center布局
-            if (ob.scrollable === true || ob.scrollx === true || ob.scrolly === true) {
-                // 不是IE用flex_wrapper_center布局
-                return BI.extend({}, ob, {type: "bi.flex_wrapper_vertical_center"});
-            }
-            return BI.extend(ob, {type: "bi.flex_vertical_center"});
-        }
-        return ob;
-
-    });
-    BI.Plugin.registerWidget("bi.float_center_adapt", function (ob) {
-        if (!BI.isIE() && isSupportFlex) {
-            // 有滚动条的情况下需要用到flex_wrapper_center布局
-            if (ob.scrollable === true || ob.scrollx === true || ob.scrolly === true) {
-                // 不是IE用flex_wrapper_center布局
-                return BI.extend({}, ob, {type: "bi.flex_wrapper_center"});
-            }
-            return BI.extend(ob, {type: "bi.flex_center"});
-        }
-        return ob;
-
-    });
 });/**
  * guy
  * 控制器
@@ -18315,6 +18268,55 @@ _.extend(BI, {
             }
         });
     }
+});// 工程配置
+BI.prepares.push(function () {
+    // 注册布局
+    var _isSupprtFlex;
+    var isSupportFlex = function () {
+        if(_isSupprtFlex == null) {
+            _isSupprtFlex = !!(BI.isSupportCss3 && BI.isSupportCss3("flex"));
+        }
+        return _isSupprtFlex;
+    };
+    BI.Plugin.registerWidget("bi.horizontal", function (ob) {
+        if (!BI.isIE() && isSupportFlex()) {
+            return BI.extend(ob, {type: "bi.flex_horizontal"});
+        }
+        return ob;
+    });
+    BI.Plugin.registerWidget("bi.center_adapt", function (ob) {
+        if (!BI.isIE() && isSupportFlex() && ob.items && ob.items.length <= 1) {
+            // 有滚动条的情况下需要用到flex_wrapper_center布局
+            if (ob.scrollable === true || ob.scrollx === true || ob.scrolly === true) {
+                // 不是IE用flex_wrapper_center布局
+                return BI.extend(ob, {type: "bi.flex_wrapper_center"});
+            }
+            return BI.extend(ob, {type: "bi.flex_center"});
+        }
+        return ob;
+    });
+    BI.Plugin.registerWidget("bi.vertical_adapt", function (ob) {
+        if (!BI.isIE() && isSupportFlex()) {
+            // 有滚动条的情况下需要用到flex_wrapper_center布局
+            if (ob.scrollable === true || ob.scrollx === true || ob.scrolly === true) {
+                // 不是IE用flex_wrapper_center布局
+                return BI.extend({}, ob, {type: "bi.flex_wrapper_vertical_center"});
+            }
+            return BI.extend(ob, {type: "bi.flex_vertical_center"});
+        }
+        return ob;
+    });
+    BI.Plugin.registerWidget("bi.float_center_adapt", function (ob) {
+        if (!BI.isIE() && isSupportFlex()) {
+            // 有滚动条的情况下需要用到flex_wrapper_center布局
+            if (ob.scrollable === true || ob.scrollx === true || ob.scrolly === true) {
+                // 不是IE用flex_wrapper_center布局
+                return BI.extend({}, ob, {type: "bi.flex_wrapper_center"});
+            }
+            return BI.extend(ob, {type: "bi.flex_center"});
+        }
+        return ob;
+    });
 });/**
  * Detect Element Resize.
  * Forked in order to guard against unsafe 'window' and 'document' references.
