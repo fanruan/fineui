20845bdd66159279369cca34873bf7212bec7125
guy <guy@finereport.com>
2018-08-29 01:26:26 +0800

    update

--- dist/fineui.js
+++ dist/fineui.js
@@ -12365,9 +12365,8 @@ _.extend(BI.OB.prototype, {
     BI.mount = function (widget, container) {
         if (container) {
             BI.Widget._renderEngine.createElement(container).append(widget.element);
-            return widget._mount(true, false, false);
         }
-        return widget._mount(true, true, false);
+        return widget._mount(true, false, false);
     };
 })();(function () {
     var kv = {};
