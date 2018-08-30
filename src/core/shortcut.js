9ef68ff58aad57ece21ffa6dd1a6e55eae46dd83
guy <guy@finereport.com>
2018-08-30 21:36:33 +0800

    update

--- src/core/shortcut.js
+++ src/core/shortcut.js
@@ -19,7 +19,7 @@
 
     BI.createWidget = function (item, options, context) {
         // 先把准备环境准备好
-        while(BI.prepares && BI.prepares.length > 0) {
+        while (BI.prepares && BI.prepares.length > 0) {
             BI.prepares.shift()();
         }
         var el, w;
