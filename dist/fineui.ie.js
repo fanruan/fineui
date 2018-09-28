cdf4ad6ab1936b38fe3617d11d9e143dffe1c6f7
guy <guy@finereport.com>
2018-09-28 22:58:29 +0800

    update

--- dist/fineui.ie.js
+++ dist/fineui.ie.js
@@ -36067,9 +36067,9 @@ BI.BasicButton = BI.inherit(BI.Single, {
             this.fireEvent(BI.Controller.EVENT_CHANGE, BI.Events.CLICK, v, this, e);
             this.fireEvent(BI.BasicButton.EVENT_CHANGE, v, this);
             if (o.action) {
-                BI.Actions.runAction(o.action, "click", o);
+                BI.Actions.runAction(o.action, "click", o, this);
             }
-            BI.Actions.runGlobalAction("click", o);
+            BI.Actions.runGlobalAction("click", o, this);
         }
     },
 
