596a2e7c37ab3a724e2c20cd2972d90ad507b018
windy <1374721899@qq.com>
2018-10-22 17:37:00 +0800

    暂时先还原一下

--- dist/bundle.ie.js
+++ dist/bundle.ie.js
@@ -30415,7 +30415,6 @@ BI.CenterAdaptLayout = BI.inherit(BI.Layout, {
         return BI.extend(BI.CenterAdaptLayout.superclass.props.apply(this, arguments), {
             baseCls: "bi-center-adapt-layout",
             columnSize: [],
-            scrollx: false,
             hgap: 0,
             vgap: 0,
             lgap: 0,
@@ -30425,33 +30424,92 @@ BI.CenterAdaptLayout = BI.inherit(BI.Layout, {
         });
     },
     render: function () {
-        var o = this.options, self = this;
         BI.CenterAdaptLayout.superclass.render.apply(this, arguments);
-        return {
-            type: "bi.horizontal",
-            verticalAlign: BI.VerticalAlign.Middle,
-            horizontalAlign: BI.HorizontalAlign.Center,
-            columnSize: o.columnSize,
-            scrollx: o.scrollx,
-            items: o.items,
-            ref: function (_ref) {
-                self.layout = _ref;
-            },
-            hgap: o.hgap,
-            vgap: o.vgap,
-            lgap: o.lgap,
-            rgap: o.rgap,
-            tgap: o.tgap,
-            bgap: o.bgap
-        };
+        this.$table = BI.Widget._renderEngine.createElement("<table>").attr({cellspacing: 0, cellpadding: 0}).css({
+            position: "relative",
+            width: "100%",
+            height: "100%",
+            "white-space": "nowrap",
+            "border-spacing": "0px",
+            border: "none",
+            "border-collapse": "separate"
+        });
+        this.$tr = BI.Widget._renderEngine.createElement("<tr>");
+        this.$tr.appendTo(this.$table);
+        this.populate(this.options.items);
+    },
+
+    _addElement: function (i, item) {
+        var o = this.options;
+        var td;
+        var width = o.columnSize[i] <= 1 ? (o.columnSize[i] * 100 + "%") : o.columnSize[i];
+        if (!this.hasWidget(this._getChildName(i))) {
+            var w = BI.createWidget(item);
+            w.element.css({position: "relative", top: "0", left: "0", margin: "0px auto"});
+            td = BI.createWidget({
+                type: "bi.default",
+                tagName: "td",
+                attributes: {
+                    width: width
+                },
+                items: [w]
+            });
+            this.addWidget(this._getChildName(i), td);
+        } else {
+            td = this.getWidgetByName(this._getChildName(i));
+            td.element.attr("width", width);
+        }
+        td.element.css({"max-width": o.columnSize[i]});
+        if (i === 0) {
+            td.element.addClass("first-element");
+        }
+        td.element.css({
+            position: "relative",
+            height: "100%",
+            "vertical-align": "middle",
+            margin: "0",
+            padding: "0",
+            border: "none"
+        });
+        if (o.vgap + o.tgap + (item.tgap || 0) + (item.vgap || 0) !== 0) {
+            w.element.css({
+                "margin-top": o.vgap + o.tgap + (item.tgap || 0) + (item.vgap || 0) + "px"
+            });
+        }
+        if (o.hgap + o.lgap + (item.lgap || 0) + (item.hgap || 0) !== 0) {
+            w.element.css({
+                "margin-left": (i === 0 ? o.hgap : 0) + o.lgap + (item.lgap || 0) + (item.hgap || 0) +"px"
+            });
+        }
+        if (o.hgap + o.rgap + (item.rgap || 0) + (item.hgap || 0) !== 0) {
+            w.element.css({
+                "margin-right": o.hgap + o.rgap + (item.rgap || 0) + (item.hgap || 0) + "px"
+            });
+        }
+        if (o.vgap + o.bgap + (item.bgap || 0) + (item.vgap || 0) !== 0) {
+            w.element.css({
+                "margin-bottom": o.vgap + o.bgap + (item.bgap || 0) + (item.vgap || 0) + "px"
+            });
+        }
+        return td;
+    },
+
+    appendFragment: function (frag) {
+        this.$tr.append(frag);
+        this.element.append(this.$table);
     },
 
     resize: function () {
         // console.log("center_adapt布局不需要resize");
     },
 
+    _getWrapper: function () {
+        return this.$tr;
+    },
+
     populate: function (items) {
-        this.layout.populate.apply(this, arguments);
+        BI.CenterAdaptLayout.superclass.populate.apply(this, arguments);
+        this._mount();
     }
 });
 BI.shortcut("bi.center_adapt", BI.CenterAdaptLayout);/**
@@ -30723,6 +30781,106 @@ BI.RightVerticalAdaptLayout = BI.inherit(BI.Layout, {
     }
 });
 BI.shortcut("bi.right_vertical_adapt", BI.RightVerticalAdaptLayout);/**
+ * 使用display:table和display:table-cell实现的horizontal布局
+ * @class BI.TableAdaptLayout
+ * @extends BI.Layout
+ */
+BI.TableAdaptLayout = BI.inherit(BI.Layout, {
+    props: function () {
+        return BI.extend(BI.TableAdaptLayout.superclass.props.apply(this, arguments), {
+            baseCls: "bi-table-center-adapt-layout",
+            columnSize: [],
+            verticalAlign: BI.VerticalAlign.Top,
+            horizontalAlign: BI.HorizontalAlign.Left,
+            hgap: 0,
+            vgap: 0,
+            lgap: 0,
+            rgap: 0,
+            tgap: 0,
+            bgap: 0
+        });
+    },
+    render: function () {
+        var o = this.options;
+        BI.TableAdaptLayout.superclass.render.apply(this, arguments);
+        this.$table = BI.Widget._renderEngine.createElement("<div>").css({
+            position: "relative",
+            display: "table",
+            height: o.verticalAlign === BI.VerticalAlign.Middle ? "100%" : "auto",
+            width: o.horizontalAlign === BI.HorizontalAlign.Center ? "100%" : "auto",
+            "white-space": "nowrap"
+        });
+        this.populate(this.options.items);
+    },
+
+    _addElement: function (i, item) {
+
+        var o = this.options;
+        var td;
+        var width = o.columnSize[i] <= 1 ? (o.columnSize[i] * 100 + "%") : o.columnSize[i];
+        if (!this.hasWidget(this._getChildName(i))) {
+            var w = BI.createWidget(item);
+            w.element.css({position: "relative", top: "0", left: "0", margin: "0px auto"});
+            td = BI.createWidget({
+                type: "bi.default",
+                width: width,
+                items: [w]
+            });
+            this.addWidget(this._getChildName(i), td);
+        } else {
+            td = this.getWidgetByName(this._getChildName(i));
+            td.element.width(width);
+        }
+        td.element.css({"max-width": o.columnSize[i] + "px"});
+        if (i === 0) {
+            td.element.addClass("first-element");
+        }
+        td.element.css({
+            position: "relative",
+            display: "table-cell",
+            "vertical-align": "middle",
+            margin: "0",
+            padding: "0",
+            height: "100%"
+        });
+        if (o.vgap + o.tgap + (item.tgap || 0) + (item.vgap || 0) !== 0) {
+            w.element.css({
+                "margin-top": o.vgap + o.tgap + (item.tgap || 0) + (item.vgap || 0) + "px"
+            });
+        }
+        if (o.hgap + o.lgap + (item.lgap || 0) + (item.hgap || 0) !== 0) {
+            w.element.css({
+                "margin-left": (i === 0 ? o.hgap : 0) + o.lgap + (item.lgap || 0) + (item.hgap || 0) + "px"
+            });
+        }
+        if (o.hgap + o.rgap + (item.rgap || 0) + (item.hgap || 0) !== 0) {
+            w.element.css({
+                "margin-right": o.hgap + o.rgap + (item.rgap || 0) + (item.hgap || 0) + "px"
+            });
+        }
+        if (o.vgap + o.bgap + (item.bgap || 0) + (item.vgap || 0) !== 0) {
+            w.element.css({
+                "margin-bottom": o.vgap + o.bgap + (item.bgap || 0) + (item.vgap || 0) + "px"
+            });
+        }
+        return td;
+    },
+
+    appendFragment: function (frag) {
+        this.$table.append(frag);
+        this.element.append(this.$table);
+    },
+
+    resize: function () {
+        // console.log("center_adapt布局不需要resize");
+    },
+
+    populate: function (items) {
+        BI.TableAdaptLayout.superclass.populate.apply(this, arguments);
+        this._mount();
+    }
+});
+BI.shortcut("bi.table_adapt", BI.TableAdaptLayout);/**
  * 垂直方向居中容器
  * @class BI.VerticalAdaptLayout
  * @extends BI.Layout
@@ -30731,7 +30889,7 @@ BI.VerticalAdaptLayout = BI.inherit(BI.Layout, {
     props: {
         baseCls: "bi-vertical-adapt-layout",
         columnSize: [],
-        scrollx: false,
+        horizontalAlign: BI.HorizontalAlign.Left,
         hgap: 0,
         vgap: 0,
         lgap: 0,
@@ -30739,27 +30897,85 @@ BI.VerticalAdaptLayout = BI.inherit(BI.Layout, {
         tgap: 0,
         bgap: 0
     },
-
     render: function () {
-        var self = this, o = this.options;
         BI.VerticalAdaptLayout.superclass.render.apply(this, arguments);
-        return {
-            type: "bi.horizontal",
-            verticalAlign: BI.VerticalAlign.Middle,
-            horizontalAlign: BI.HorizontalAlign.Left,
-            columnSize: o.columnSize,
-            items: o.items,
-            scrollx: o.scrollx,
-            ref: function (_ref) {
-                self.layout = _ref;
-            },
-            hgap: o.hgap,
-            vgap: o.vgap,
-            lgap: o.lgap,
-            rgap: o.rgap,
-            tgap: o.tgap,
-            bgap: o.bgap
-        };
+        var o = this.options;
+        this.$table = BI.Widget._renderEngine.createElement("<table>").attr({cellspacing: 0, cellpadding: 0}).css({
+            position: "relative",
+            width: o.horizontalAlign === BI.HorizontalAlign.Stretch ? "100%" : "auto",
+            height: "100%",
+            "white-space": "nowrap",
+            "border-spacing": "0px",
+            border: "none",
+            "border-collapse": "separate"
+        });
+        this.$tr = BI.Widget._renderEngine.createElement("<tr>");
+        this.$tr.appendTo(this.$table);
+        this.populate(this.options.items);
+    },
+
+    _addElement: function (i, item) {
+        var o = this.options;
+        var td;
+        var width = o.columnSize[i] <= 1 ? (o.columnSize[i] * 100 + "%") : o.columnSize[i];
+        if (!this.hasWidget(this._getChildName(i))) {
+            var w = BI.createWidget(item);
+            w.element.css({position: "relative", top: "0", left: "0", margin: "0px auto"});
+            td = BI.createWidget({
+                type: "bi.default",
+                tagName: "td",
+                attributes: {
+                    width: width
+                },
+                items: [w]
+            });
+            this.addWidget(this._getChildName(i), td);
+        } else {
+            td = this.getWidgetByName(this._getChildName(i));
+            td.element.attr("width", width);
+        }
+
+        if (i === 0) {
+            td.element.addClass("first-element");
+        }
+        td.element.css({
+            position: "relative",
+            height: "100%",
+            "vertical-align": "middle",
+            margin: "0",
+            padding: "0",
+            border: "none"
+        });
+        if (o.vgap + o.tgap + (item.tgap || 0) + (item.vgap || 0) !== 0) {
+            w.element.css({
+                "margin-top": o.vgap + o.tgap + (item.tgap || 0) + (item.vgap || 0) + "px"
+            });
+        }
+        if (o.hgap + o.lgap + (item.lgap || 0) + (item.hgap || 0) !== 0) {
+            w.element.css({
+                "margin-left": (i === 0 ? o.hgap : 0) + o.lgap + (item.lgap || 0) + (item.hgap || 0) +"px"
+            });
+        }
+        if (o.hgap + o.rgap + (item.rgap || 0) + (item.hgap || 0) !== 0) {
+            w.element.css({
+                "margin-right": o.hgap + o.rgap + (item.rgap || 0) + (item.hgap || 0) + "px"
+            });
+        }
+        if (o.vgap + o.bgap + (item.bgap || 0) + (item.vgap || 0) !== 0) {
+            w.element.css({
+                "margin-bottom": o.vgap + o.bgap + (item.bgap || 0) + (item.vgap || 0) + "px"
+            });
+        }
+        return td;
+    },
+
+    appendFragment: function (frag) {
+        this.$tr.append(frag);
+        this.element.append(this.$table);
+    },
+
+    _getWrapper: function () {
+        return this.$tr;
     },
 
     resize: function () {
@@ -30767,7 +30983,8 @@ BI.VerticalAdaptLayout = BI.inherit(BI.Layout, {
     },
 
     populate: function (items) {
-        this.layout.populate.apply(this, arguments);
+        BI.VerticalAdaptLayout.superclass.populate.apply(this, arguments);
+        this._mount();
     }
 });
 BI.shortcut("bi.vertical_adapt", BI.VerticalAdaptLayout);/**
@@ -58403,7 +58620,7 @@ BI.DatePicker = BI.inherit(BI.Widget, {
                 items: [{
                     el: {
                         type: "bi.horizontal",
-                        width: 110,
+                        width: 120,
                         rgap: 10,
                         items: [{
                             el: this.year,
