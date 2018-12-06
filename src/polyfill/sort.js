81d6eebc3ca893b5a67e9c378a36706c2cb10ea3
windy <1374721899@qq.com>
2018-12-06 12:53:24 +0800

    BI-36544 chrome下sort方法的问题

--- src/polyfill/sort.js
+++ src/polyfill/sort.js
@@ -1,7 +1,10 @@
 // 修复ie9下sort方法的bug
+// IE的sort 需要显示声明返回-1, 0, 1三种比较结果才可正常工作，而Chrome， Firefox中可以直接返回true, false等
+// BI-36544 sort提供的参数就可以自定义返回值，不需要特别控制。这边使用冒泡相较于快排可能慢了点。
+// 这边先将webkit的限制去掉
 !function (window) {
     var ua = window.navigator.userAgent.toLowerCase(),
-        reg = /msie|applewebkit.+safari/;
+        reg = /msie/;
     if (reg.test(ua)) {
         var _sort = Array.prototype.sort;
         Array.prototype.sort = function (fn) {
