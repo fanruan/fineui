a12831de05ee7b44870409acd2f2d0384e01f93c
Young <young@finereport.com>
2018-12-18 14:43:16 +0800

    update 去掉pane中无效方法

--- dist/base.js
+++ dist/base.js
@@ -218,20 +218,6 @@ BI.Pane = BI.inherit(BI.Widget, {
         });
     },
 
-    _init: function () {
-        BI.Pane.superclass._init.apply(this, arguments);
-        if (this.__async) {
-            this.loading();
-        }
-    },
-
-    _render: function () {
-        BI.Pane.superclass._render.apply(this, arguments);
-        if (this.__async) {
-            this.loaded();
-        }
-    },
-
     _assertTip: function () {
         var o = this.options;
         if (!this._tipText) {
