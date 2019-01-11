a510ab32d7de4a9753c604b114491fddb07d5199
windy <1374721899@qq.com>
2019-01-11 10:18:21 +0800

    update

--- src/case/combo/bubblecombo/popup.bubble.js
+++ src/case/combo/bubblecombo/popup.bubble.js
@@ -97,7 +97,7 @@ BI.shortcut("bi.bubble_bar_popup_view", BI.BubblePopupBarView);
  * @class BI.TextBubblePopupBarView
  * @extends BI.BubblePopupView
  */
-BI.TextBubblePopupBarView = BI.inherit(BI.BubblePopupView, {
+BI.TextBubblePopupBarView = BI.inherit(BI.Widget, {
 
     props: function () {
         return {
@@ -135,60 +135,16 @@ BI.TextBubblePopupBarView = BI.inherit(BI.BubblePopupView, {
                 self.popup = this;
             },
             el: {
-
+                type: "bi.label",
+                text: o.text,
+                whiteSpace: "normal",
+                textAlign: "left",
+                ref: function () {
+                    self.text = this;
+                }
             },
             buttons: buttons
         };
-    },
-
-    _createToolBar: function () {
-        var o = this.options, self = this;
-
-        var items = [];
-        BI.each(o.buttons, function (i, buttonOpt) {
-            if (BI.isWidget(buttonOpt)) {
-                items.push(buttonOpt);
-            } else {
-                items.push(BI.extend({
-                    type: "bi.button",
-                    height: 24,
-                    handler: function (v) {
-                        self.fireEvent(BI.BubblePopupBarView.EVENT_CLICK_TOOLBAR_BUTTON, v);
-                    }
-                }, buttonOpt));
-            }
-        });
-        return BI.createWidget({
-            type: "bi.center",
-            height: 44,
-            rgap: 15,
-            items: [{
-                type: "bi.right_vertical_adapt",
-                lgap: 10,
-                items: items
-            }]
-        });
-    },
-
-    _createView: function () {
-        var self = this, o = this.options;
-
-        var button =  BI.createWidget({
-            type: "bi.label",
-            cls: "bar-popup-container",
-            text: o.text,
-            whiteSpace: "normal",
-            textAlign: "left",
-            ref: function () {
-                self.text = this;
-            },
-            hgap: 15,
-            tgap: 10
-        });
-
-        button.element.css("min-height", o.minHeight - 44);
-
-        return button;
     },
 
     populate: function (v) {
