b839e916970195cc61231fded31817b2f0071186
iapyang <yklorse@gmail.com>
2019-03-26 16:15:15 +0800

    BI-42297 fix: items未定义

--- dist/2.0/fineui.ie.js
+++ dist/2.0/fineui.ie.js
@@ -34075,7 +34075,7 @@ BI.CenterLayout = BI.inherit(BI.Layout, {
     render: function () {
         BI.CenterLayout.superclass.render.apply(this, arguments);
         var self = this, o = this.options;
-        var list = [];
+        var list = [], items = o.items;
         BI.each(items, function (i) {
             list.push({
                 column: i,
@@ -34148,7 +34148,7 @@ BI.FloatCenterLayout = BI.inherit(BI.Layout, {
     },
     render: function () {
         BI.FloatCenterLayout.superclass.render.apply(this, arguments);
-        var self = this, o = this.options;
+        var self = this, o = this.options, items = o.items;
         var list = [], width = 100 / items.length;
         BI.each(items, function (i) {
             var widget = BI.createWidget({
@@ -34222,7 +34222,7 @@ BI.HorizontalCenterLayout = BI.inherit(BI.Layout, {
     },
     render: function () {
         BI.HorizontalCenterLayout.superclass.render.apply(this, arguments);
-        var self = this, o = this.options;
+        var self = this, o = this.options, items = o.items;
         var list = [];
         BI.each(items, function (i) {
             list.push({
@@ -34296,7 +34296,7 @@ BI.VerticalCenterLayout = BI.inherit(BI.Layout, {
 
     render: function () {
         BI.VerticalCenterLayout.superclass.render.apply(this, arguments);
-        var self = this, o = this.options;
+        var self = this, o = this.options, items = o.items;
         var list = [];
         BI.each(items, function (i) {
             list.push({
