5be9c7e4c4ac291cebd79c56d4b2daa52fd99c30
iapyang <yklorse@gmail.com>
2019-07-04 14:13:06 +0800

    KERNEL-801 feat: 输出BI

---
+++ typescript/index.ts
@@ -0,0 +1,11 @@
+import { _addI18n, _i18nText } from "./core/i18n";
+import { _OB } from "./core/ob";
+
+export declare module BI {
+    namespace i18n {
+        const addI18n: _addI18n;
+        const i18nText: _i18nText;
+    }
+
+    const OB: _OB;
+}
