2e2e73d02671ec9f387792a4b3e5239b1bfcf4c9
iapyang <yklorse@gmail.com>
2019-07-19 10:50:59 +0800

    REPORT-19560 feat: 补充类

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,9 +1,19 @@
 import { _i18n } from "./core/i18n";
-import { OBConstructor } from "./core/ob";
+import { _OB } from "./core/ob";
 import { _func } from "./core/func";
-import { WidgetConstructor } from "./core/widget";
+import { _Widget, _WidgetStatic } from "./core/widget";
+import { _Single } from "./base/single/single";
+import { _BasicButton } from "./base/single/button/button.basic";
+
+type Constructor<T extends {}> = T & {
+    new(config: any): T;
+    (config: any): T;
+    readonly prototype: T;
+}
 
 export interface _BI extends _func, _i18n {
-    OB: OBConstructor;
-    Widget: WidgetConstructor;
+    OB: Constructor<_OB>;
+    Widget: Constructor<_Widget> & _WidgetStatic;
+    Single: Constructor<_Single>;
+    BasicButton: Constructor<_BasicButton>;
 }
