b1e147e2723b34951ab006475589067592388a65
alan <alan@fanruan.com>
2019-07-26 10:09:56 +0800

    feat: KERNEL-800 core/base

--- typescript/index.ts
+++ typescript/index.ts
@@ -3,6 +3,7 @@ import { _OB } from "./core/ob";
 import { _func } from "./core/func";
 import { _Widget, _WidgetStatic } from "./core/widget";
 import { _Single } from "./base/single/single";
+import { _base } from "./core/base";
 import { _BasicButton } from "./base/single/button/button.basic";
 
 type Constructor<T extends {}> = T & {
@@ -11,7 +12,7 @@ type Constructor<T extends {}> = T & {
     readonly prototype: T;
 }
 
-export interface _BI extends _func, _i18n {
+export interface _BI extends _func, _i18n, _base {
     OB: Constructor<_OB>;
     Widget: Constructor<_Widget> & _WidgetStatic;
     Single: Constructor<_Single>;
