4c5cfd49fa375c72668b93b0a60449011357f548
iapyang <yklorse@gmail.com>
2019-09-04 19:52:23 +0800

    refactor: 去掉declear

--- typescript/core/func/array.ts
+++ typescript/core/func/array.ts
@@ -1,8 +1,8 @@
-export declare type _pushArray = (sArray: any[], array: any[]) => void;
+export type _pushArray = (sArray: any[], array: any[]) => void;
 
-export declare type _pushDistinct = (sArray: any[], obj: any) => void;
+export type _pushDistinct = (sArray: any[], obj: any) => void;
 
-export declare type _pushDistinctArray = (sArray: any[], array: any[]) => void;
+export type _pushDistinctArray = (sArray: any[], array: any[]) => void;
 
 declare type _array = {
     pushArray: _pushArray;
--- typescript/core/i18n.ts
+++ typescript/core/i18n.ts
@@ -1,8 +1,8 @@
-export declare type _addI18n = (v: string | object) => string;
+export type _addI18n = (v: string | object) => string;
 
-export declare type _i18nText = (key: string, ..._args: any[]) => string;
+export type _i18nText = (key: string, ..._args: any[]) => string;
 
-export declare type _i18n = {
+export type _i18n = {
     addI18n: _addI18n;
     i18nText: _i18nText;
 }
