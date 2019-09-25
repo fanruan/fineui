63acc5c4ae6418d816dab13a578aed209c3941a2
windy <1374721899@qq.com>
2019-09-25 12:13:05 +0800

    无JIRA任务 文本列表复制粘贴的时候要发事件

--- src/widget/multiselectlist/multiselectlist.insert.nobar.js
+++ src/widget/multiselectlist/multiselectlist.insert.nobar.js
@@ -142,6 +142,7 @@ BI.MultiSelectInsertNoBarList = BI.inherit(BI.Single, {
                                 self.adapter.setValue(self.storeValue);
                                 assertShowValue();
                             }
+                            self.fireEvent(BI.MultiSelectInsertList.EVENT_CHANGE);
                         });
                     }
                 }
