870f823aeb1256fb352fec4dc4f2328a1b896ee6
windy <1374721899@qq.com>
2019-11-29 11:22:34 +0800

    KERNEL-2278 fix: 测试的一些问题修复

--- src/widget/dynamicdatetime/dynamicdatetime.combo.js
+++ src/widget/dynamicdatetime/dynamicdatetime.combo.js
@@ -275,12 +275,14 @@ BI.DynamicDateTimeCombo = BI.inherit(BI.Single, {
         var o = this.options;
         o.minDate = minDate;
         this.trigger.setMinDate(minDate);
+        this.popup && this.popup.setMinDate(minDate);
     },
 
     setMaxDate: function (maxDate) {
         var o = this.options;
         o.maxDate = maxDate;
         this.trigger.setMaxDate(maxDate);
+        this.popup && this.popup.setMaxDate(maxDate);
     },
 
     setValue: function (v) {
