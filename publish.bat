2b1725d6e2a30f816ab8860b1003b1ba148c06e8
iapyang <yklorse@gmail.com>
2020-01-10 14:43:12 +0800

    Revert "chore: remove prepublish"
    This reverts commit b8c6819403a047d5e53ea2b846b7c0839f066e12.

--- publish.bat
+++
@@ -1,5 +0,0 @@
-For /f "delims=." %%A in (
-    'wmic os get LocalDateTime^|findstr ^^20'
-) Do Set DT=%%A
-Set "TIMESTAMP=%DT:~0,4%/%DT:~4,2%/%DT:~6,2%/%DT:~8,2%/%DT:~10,2%/%DT:~12,2%"
-npm publish --tag=%TIMESTAMP%
