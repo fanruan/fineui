1cd60a49d301048b9fb29645fd68d0daa33188b6
Kira <Kira@fanruan.com>
2020-02-13 16:47:50 +0800

    KERNEL-3025 refactor: 增加bi.tab声明

---
+++ typescript/base/combination/tab.ts
@@ -0,0 +1,17 @@
+import { _Widget } from "../../core/widget";
+
+export interface _Tab extends _Widget {
+    setSelect(v: string | number): void;
+
+    removeTab(v: string | number): void;
+
+    getSelect(): string | number;
+
+    getSelectedTab(): _Widget;
+
+    getTab(v: string | number): _Widget;
+}
+
+export interface _TabStatic {
+    EVENT_CHANGE: string;
+}
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,20 +1,21 @@
-import * as decorator from "./core/decorator/decorator";
-
-import { _i18n } from "./core/i18n";
-import { _OB } from "./core/ob";
-import { _func } from "./core/func";
-import { _Widget, _WidgetStatic } from "./core/widget";
-import { _Single } from "./base/single/single";
-import { _base } from "./core/base";
+import { _Tab, _TabStatic } from "./base/combination/tab";
+import { _Pane, _PaneStatic } from "./base/pane";
 import { _BasicButton, _BasicButtonStatic } from "./base/single/button/button.basic";
+import { _Single } from "./base/single/single";
 import { _Trigger } from "./base/single/trigger/trigger";
+import { _LoadingPane } from "./case/loading/loading_pane";
 import { _Action, _ActionFactory } from "./core/action/action";
 import { _ShowAction } from "./core/action/action.show";
+import { _base } from "./core/base";
 import { _Behavior, _BehaviorFactory } from "./core/behavior/behavior";
 import { _HighlightBehavior } from "./core/behavior/behavior.highlight";
 import { _RedMarkBehavior } from "./core/behavior/behavior.redmark";
-import { _Pane, _PaneStatic } from "./base/pane";
-import { _LoadingPane } from "./case/loading/loading_pane";
+import * as decorator from "./core/decorator/decorator";
+import { _func } from "./core/func";
+import { _i18n } from "./core/i18n";
+import { _OB } from "./core/ob";
+import { _Widget, _WidgetStatic } from "./core/widget";
+
 
 type ClassConstructor<T extends {}> = T & {
     new(config: any): T;
@@ -37,6 +38,7 @@ export interface BI extends _func, _i18n, _base {
     RedMarkBehavior: ClassConstructor<_RedMarkBehavior>;
     Pane: ClassConstructor<_Pane> & _PaneStatic;
     LoadingPane: ClassConstructor<_LoadingPane>;
+    Tab: ClassConstructor<_Tab> & _TabStatic;
     Decorators: typeof decorator;
 }
 
