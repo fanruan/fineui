c863894f08c8fad6a913dd727b6c0aa23c3bd44c
windy <1374721899@qq.com>
2020-03-02 15:43:41 +0800

    BI-52072 test: 单测

---
+++ src/case/combo/textvaluecombo/__test__/combo.textvalue.test.js
@@ -0,0 +1,118 @@
+/**
+ * @author windy
+ * @version 2.0
+ * Created by windy on 2020/3/2
+ */
+
+describe("textvaluecombo", function () {
+
+    /**
+     * test_author_windy
+     */
+    it("测试setValue", function () {
+        var combo = BI.Test.createWidget({
+            type: "bi.text_value_combo",
+            text: "默认值",
+            value: 22,
+            width: 300,
+            items: [{
+                text: "MVC-1",
+                iconCls: "date-font",
+                value: 1
+            }, {
+                text: "MVC-2",
+                iconCls: "search-font",
+                value: 2
+            }, {
+                text: "MVC-3",
+                iconCls: "pull-right-font",
+                value: 3
+            }]
+        });
+        combo.setValue(2);
+        expect(combo.getValue()[0]).to.equal(2);
+    });
+
+    /**
+     * test_author_windy
+     */
+    it("测试populate", function (done) {
+        var combo = BI.Test.createWidget({
+            type: "bi.text_value_combo",
+            text: "默认值",
+            value: 22,
+            width: 300
+        });
+        combo.populate([{
+            text: "MVC-1",
+            iconCls: "date-font",
+            value: 1
+        }, {
+            text: "MVC-2",
+            iconCls: "search-font",
+            value: 2
+        }, {
+            text: "MVC-3",
+            iconCls: "pull-right-font",
+            value: 3
+        }]);
+        combo.setValue(2);
+        expect(combo.getValue()[0]).to.equal(2);
+        done();
+    });
+
+    /**
+     * test_author_windy
+     */
+    it("测试small_setValue", function () {
+        var combo = BI.Test.createWidget({
+            type: "bi.small_text_value_combo",
+            text: "默认值",
+            value: 22,
+            width: 300,
+            items: [{
+                text: "MVC-1",
+                iconCls: "date-font",
+                value: 1
+            }, {
+                text: "MVC-2",
+                iconCls: "search-font",
+                value: 2
+            }, {
+                text: "MVC-3",
+                iconCls: "pull-right-font",
+                value: 3
+            }]
+        });
+        combo.setValue(2);
+        expect(combo.getValue()[0]).to.equal(2);
+    });
+
+    /**
+     * test_author_windy
+     */
+    it("测试small_populate", function (done) {
+        var combo = BI.Test.createWidget({
+            type: "bi.small_text_value_combo",
+            text: "默认值",
+            value: 22,
+            width: 300
+        });
+        combo.populate([{
+            text: "MVC-1",
+            iconCls: "date-font",
+            value: 1
+        }, {
+            text: "MVC-2",
+            iconCls: "search-font",
+            value: 2
+        }, {
+            text: "MVC-3",
+            iconCls: "pull-right-font",
+            value: 3
+        }]);
+        combo.setValue(2);
+        expect(combo.getValue()[0]).to.equal(2);
+        done();
+    });
+});
