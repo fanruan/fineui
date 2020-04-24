1bb90a1c4dc45761d53578c80696bb8a30af5bb5
Kobi <Kobi@fanruan.com>
2020-04-24 14:47:35 +0800

    KERNEL-3911 用BI.concat替换concat

--- demo/js/west.js
+++ demo/js/west.js
@@ -37,7 +37,7 @@ Demo.West = BI.inherit(BI.Widget, {
                     },
                     onSearch: function (op, callback) {
                         var result = BI.Func.getSearchResult(Demo.CONFIG, op.keyword, "text");
-                        var items = result.match.concat(result.find);
+                        var items = BI.concat(result.match, result.find);
                         var children = [];
                         BI.each(items, function (index, item) {
                             var childList = BI.Func.getSearchResult(Demo.CONFIG, item.id, "pId");
@@ -47,7 +47,7 @@ Demo.West = BI.inherit(BI.Widget, {
                                 }
                             });
                         });
-                        items = items.concat(children);
+                        items = BI.concat(items, children);
                         callback(items);
                     }
                 }],
