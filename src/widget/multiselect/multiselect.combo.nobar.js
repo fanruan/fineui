6c78bf6846b6e81a956e5df5f459307f2e819009
windy <1374721899@qq.com>
2020-05-15 12:45:40 +0800

    BI-65448 fix: 点击查看已选不需要刷新计数

--- src/widget/multiselect/multiselect.combo.nobar.js
+++ src/widget/multiselect/multiselect.combo.nobar.js
@@ -178,7 +178,7 @@ BI.MultiSelectNoBarCombo = BI.inherit(BI.Single, {
         this.combo.on(BI.Combo.EVENT_BEFORE_POPUPVIEW, function () {
             this.setValue(self.storeValue);
             BI.nextTick(function () {
-                self.populate();
+                self._populate();
             });
         });
         // 当退出的时候如果还在处理请求，则等请求结束后再对外发确定事件
@@ -414,6 +414,10 @@ BI.MultiSelectNoBarCombo = BI.inherit(BI.Single, {
         this.popup.setStartValue(value);
     },
 
+    _populate: function () {
+        this.combo.populate.apply(this.combo, arguments);
+    },
+
     setValue: function (v) {
         this.storeValue = {
             type: BI.Selection.Multi,
@@ -428,7 +432,7 @@ BI.MultiSelectNoBarCombo = BI.inherit(BI.Single, {
     },
 
     populate: function () {
-        this.combo.populate.apply(this.combo, arguments);
+        this._populate.apply(this, arguments);
         this.numberCounter.populateSwitcher.apply(this.numberCounter, arguments);
     }
 });
