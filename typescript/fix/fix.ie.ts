8e9d44ac130afcf06743dc3fd223c3325f211c58
iapyang <yklorse@gmail.com>
2020-06-04 14:32:05 +0800

    chore: fix打包

---
+++ typescript/fix/fix.ie.ts
@@ -0,0 +1,5 @@
+// @ts-ignore
+import * as Fix from "../../dist/fix/fix.ie.js";
+
+// @ts-ignore
+_global.Fix = Fix;
---
+++ typescript/fix/fix.ts
@@ -0,0 +1,5 @@
+// @ts-ignore
+import * as Fix from "../../dist/fix/fix.js";
+
+// @ts-ignore
+_global.Fix = Fix;
