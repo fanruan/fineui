c092bf2cf8af73615d5b82f91c5439f97ad118df
guy <guy@finereport.com>
2020-09-04 23:54:50 +0800

    bugfix

--- src/base/single/tip/tip.bubble.js
+++ src/base/single/tip/tip.bubble.js
@@ -34,7 +34,7 @@ BI.Bubble = BI.inherit(BI.Tip, {
         var o = this.options;
         return (this.text = BI.createWidget({
             type: "bi.label",
-            cls: "bubble-text" + (" bubble-" + o.level),
+            cls: "bubble-text" + " bubble-" + o.level,
             text: o.text,
             hgap: 5,
             height: 18
@@ -107,3 +107,106 @@ BI.Bubble = BI.inherit(BI.Tip, {
 });
 
 BI.shortcut("bi.bubble", BI.Bubble);
+
+BI.BubbleView = BI.inherit(BI.Single, {
+    _defaultConfig: function () {
+        return BI.extend(BI.Bubble.superclass._defaultConfig.apply(this, arguments), {
+            extraCls: "bi-bubble",
+            direction: "top",
+            text: "",
+            level: "error",
+            height: 18
+        });
+    },
+    _init: function () {
+        BI.BubbleView.superclass._init.apply(this, arguments);
+        var fn = function (e) {
+            e.stopPropagation();
+            e.stopEvent();
+            return false;
+        };
+        this.element.bind({click: fn, mousedown: fn, mouseup: fn, mouseover: fn, mouseenter: fn, mouseleave: fn, mousemove: fn});
+        BI.createWidget({
+            type: "bi.left",
+            element: this,
+            items: [this["_" + this.options.direction]()]
+        });
+    },
+
+    _createBubbleText: function () {
+        var o = this.options;
+        return (this.text = BI.createWidget({
+            type: "bi.label",
+            cls: "bubble-text" + " bubble-" + o.level,
+            text: o.text,
+            hgap: 5,
+            height: 18
+        }));
+    },
+
+    _top: function () {
+        return BI.createWidget({
+            type: "bi.vertical",
+            items: [{
+                el: this._createBubbleText(),
+                height: 18
+            }, {
+                el: {
+                    type: "bi.layout"
+                },
+                height: 3
+            }]
+        });
+    },
+
+    _bottom: function () {
+        return BI.createWidget({
+            type: "bi.vertical",
+            items: [{
+                el: {
+                    type: "bi.layout"
+                },
+                height: 3
+            }, {
+                el: this._createBubbleText(),
+                height: 18
+            }]
+        });
+    },
+
+    _left: function () {
+        return BI.createWidget({
+            type: "bi.right",
+            items: [{
+                el: {
+                    type: "bi.layout",
+                    width: 3,
+                    height: 18
+                }
+            }, {
+                el: this._createBubbleText()
+            }]
+        });
+    },
+
+    _right: function () {
+        return BI.createWidget({
+            type: "bi.left",
+            items: [{
+                el: {
+                    type: "bi.layout",
+                    width: 3,
+                    height: 18
+                }
+            }, {
+                el: this._createBubbleText()
+            }]
+        });
+    },
+
+    setText: function (text) {
+        this.text.setText(text);
+    }
+});
+
+BI.shortcut("bi.bubble_view", BI.BubbleView);
