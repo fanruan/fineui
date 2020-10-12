50b75ee20b2cb981934dd796d564af0a98b0fefc
data <dep@finereport.com>
2020-10-12 09:42:36 +0800

    auto upgrade version to 2.0.20201012094154

--- dist/utils.js
+++ dist/utils.js
@@ -1,4 +1,4 @@
-/*! time: 2020-10-12 09:00:19 */
+/*! time: 2020-10-12 09:40:26 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
