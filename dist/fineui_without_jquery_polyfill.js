8266ed6801cb4f4f88d4817ba3d305cbfe88031e
data <dep@finereport.com>
2020-10-29 20:42:16 +0800

    auto upgrade version to 2.0.20201029204159

--- dist/fineui_without_jquery_polyfill.js
+++ dist/fineui_without_jquery_polyfill.js
@@ -1,4 +1,4 @@
-/*! time: 2020-10-29 11:00:29 */
+/*! time: 2020-10-29 20:40:26 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
