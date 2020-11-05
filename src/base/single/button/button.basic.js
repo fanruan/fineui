4a5a60aad5e19c6b56fe63eb2562ea63e5d3932b
imp <imp@fanruan.com>
2020-11-05 14:49:08 +0800

    无jira任务 代码优化

--- src/base/single/button/button.basic.js
+++ src/base/single/button/button.basic.js
@@ -298,8 +298,8 @@ BI.BasicButton = BI.inherit(BI.Single, {
                     this.setSelected(!this.isSelected()));
         }
         if (this.isValid()) {
-            o.handler.call(this, this.getValue(), this, e);
             var v = this.getValue();
+            o.handler.call(this, v, this, e);
             this.fireEvent(BI.Controller.EVENT_CHANGE, BI.Events.CLICK, v, this, e);
             this.fireEvent(BI.BasicButton.EVENT_CHANGE, v, this);
             if (o.action) {
