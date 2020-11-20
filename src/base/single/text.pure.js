ebfe202efbc1781d9d2cd3986f6c97788a04cc3e
guy <guy@finereport.com>
2020-11-20 19:46:14 +0800

    增加一个纯文本组件

---
+++ src/base/single/text.pure.js
@@ -0,0 +1,43 @@
+/**
+ * 没有html标签的纯文本
+ */
+!(function () {
+    BI.PureText = BI.inherit(BI.Widget, {
+
+        props: {
+            tagName: null
+        },
+
+        render: function () {
+            var self = this, o = this.options;
+            var text = this._getShowText();
+            if (BI.isKey(text)) {
+                this.setText(text);
+            } else if (BI.isKey(o.value)) {
+                this.setText(o.value);
+            }
+        },
+
+        _getShowText: function () {
+            var o = this.options;
+            var text = BI.isFunction(o.text) ? o.text() : o.text;
+            text = BI.isKey(text) ? text : o.value;
+            if (!BI.isKey(text)) {
+                return "";
+            }
+            return BI.Text.formatText(text + "");
+        },
+
+        setValue: function (value) {
+            this.options.value = value;
+            this.setText(value);
+        },
+
+        setText: function (text) {
+            this.options.text = BI.isNotNull(text) ? text : "";
+            this.text.element.__textKeywordMarked__(this._getShowText());
+        }
+    });
+    BI.shortcut("bi.pure_text", BI.PureText);
+}());
+
