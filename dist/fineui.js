3d1f857d575eec3d19fda9dd8c4cc12e14ea2935
data <dep@finereport.com>
2020-11-27 17:03:10 +0800

    auto upgrade version to 2.0.20201127170237

--- dist/fineui.js
+++ dist/fineui.js
@@ -1,4 +1,4 @@
-/*! time: 2020-11-27 10:11:48 */
+/*! time: 2020-11-27 17:00:25 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -41913,8 +41913,7 @@ BI.YearPopup = BI.inherit(BI.Widget, {
                 items: [this.backBtn, this.preBtn]
             },
             cardCreator: BI.bind(this._createYearCalendar, this),
-
-            afterCardShow: function () {
+            afterCardCreated: function () {
                 this.setValue(self.selectedYear);
                 var calendar = this.getSelectedCard();
                 calendar && self.backBtn.setEnable(!calendar.isFrontYear());
