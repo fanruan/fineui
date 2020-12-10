c3c0ec6e120e861b4676642bb31f68f0c7d5bc5b
imp <imp@fanruan.com>
2020-12-10 16:21:44 +0800

    无jira任务 添加描述

--- typescript/index.ts
+++ typescript/index.ts
@@ -43,6 +43,7 @@ import { _Layout } from "./core/wrapper/layout";
 import { _AbsoluteLayout } from "./core/wrapper/layout/layout.absolute";
 import { _HTapeLayout, _VTapeLayout } from "./core/wrapper/layout/layout.tape";
 import { _VerticalLayout } from "./core/wrapper/layout/layout.vertical";
+import { _DefaultLayout } from "./core/wrapper/layout/layout.default";
 import { _DownListCombo, _DownListComboStatic } from "./widget/downlist/combo.downlist";
 
 
@@ -97,6 +98,7 @@ export interface BI extends _func, _i18n, _base, _inject {
     Iframe: ClassConstructor<_Iframe>;
     AbsoluteLayout: ClassConstructor<_AbsoluteLayout>;
     VerticalLayout: ClassConstructor<_VerticalLayout>;
+    DefaultLayout: ClassConstructor<_DefaultLayout>;
     Input: ClassConstructor<_Input> & _InputStatic;
     SearchTextValueCombo: ClassConstructor<_SearchTextValueCombo> & _SearchTextValueComboStatic;
 }
