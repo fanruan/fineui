f20c33015c5732b2f852eb9723f5fe654b947743
alan <langwdalan@gmail.com>
2020-12-11 14:48:55 +0800

    去除无用代码

--- typescript/base/single/icon/icon.ts
+++ typescript/base/single/icon/icon.ts
@@ -2,8 +2,6 @@ import { Single } from "../single";
 
 export declare class Icon extends Single {
     static xtype: string;
-    
-    _defaultConfig(..._args: any[]): { [key: string]: any } | {};
 
-    _init(): void;
+    _defaultConfig(..._args: any[]): { [key: string]: any } | {};
 }
