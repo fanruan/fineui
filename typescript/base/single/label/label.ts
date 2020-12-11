30fdf00b06225c1893874d155e51390a14fdde51
youki <Youki@fanruan.com>
2020-12-11 16:05:33 +0800

    KERNEL-6465 补充类型

--- typescript/base/single/label/label.ts
+++ typescript/base/single/label/label.ts
@@ -1,6 +1,12 @@
-import { _AbstractLabel } from "./abstract.label";
+import { _AbstractLabel, AbstractLabel } from "./abstract.label";
 
 export interface _Label extends _AbstractLabel {
+    doRedMark(...args: any[]): void;
+
+    unRedMark(...args: any[]): void
+}
+
+export declare class Label extends AbstractLabel {
     doRedMark(...args: any[]): void;
 
     unRedMark(...args: any[]): void
