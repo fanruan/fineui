9e128444f1e352415b429c546cb9c6cea3b9f7b3
data <dep@finereport.com>
2020-12-14 17:23:42 +0800

    auto upgrade version to 2.0.20201214172227

---
+++ dist/lib/core/wrapper/layout/adapt/adapt.vertical.d.ts
@@ -0,0 +1,4 @@
+import { Layout } from "typescript";
+export declare class VerticalAdaptLayout extends Layout {
+    static xtype: string;
+}
---
+++ dist/lib/core/wrapper/layout/layout.flow.d.ts
@@ -0,0 +1,4 @@
+import { Layout } from "../layout";
+export declare class FloatLeftLayout extends Layout {
+    static xtype: string;
+}
