8b63f306ed1485ae22c12c286f6df64ec86f1871
iapyang <yklorse@gmail.com>
2020-12-17 18:16:54 +0800

    refactor: 修改类型

--- typescript/base/tree/ztree/asynctree.ts
+++ typescript/base/tree/ztree/asynctree.ts
@@ -3,6 +3,8 @@ import { TreeView, TreeValue } from "./treeview";
 export declare class AsyncTree extends TreeView {
     static xtype: string;
 
+    nodes: any[];
+
     _beforeExpandNode<T>(treeId: string, treeNode: T): void;
 
     _join(valueA: TreeValue, valueB: TreeValue): TreeValue;
