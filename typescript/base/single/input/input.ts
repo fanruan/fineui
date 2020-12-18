cd1312cca126ec96417abc2da6bee88afa1f2ef6
iapyang <yklorse@gmail.com>
2020-12-18 11:35:12 +0800

    refactor: fineui中非标准写法补齐

--- typescript/base/single/input/input.ts
+++ typescript/base/single/input/input.ts
@@ -1,4 +1,4 @@
-import { _Single } from "../single";
+import { Single, _Single } from "../single";
 
 export interface _Input extends _Single {
     onClick(): void;
@@ -42,4 +42,47 @@ export interface _InputStatic {
     EVENT_ERROR: string;
     EVENT_ENTER: string;
     EVENT_RESTRICT: string;
+}
+
+export declare class Input extends Single {
+    static xtype: string;
+    static EVENT_CHANGE: string;
+    static EVENT_FOCUS: string;
+    static EVENT_CLICK: string;
+    static EVENT_BLUR: string;
+    static EVENT_KEY_DOWN: string;
+    static EVENT_QUICK_DOWN: string;
+    static EVENT_SPACE: string;
+    static EVENT_BACKSPACE: string;
+    static EVENT_START: string;
+    static EVENT_PAUSE: string;
+    static EVENT_STOP: string;
+    static EVENT_CHANGE_CONFIRM: string;
+    static EVENT_CONFIRM: string;
+    static EVENT_REMOVE: string;
+    static EVENT_EMPTY: string;
+    static EVENT_VALID: string;
+    static EVENT_ERROR: string;
+    static EVENT_ENTER: string;
+    static EVENT_RESTRICT: string;
+
+    onClick(): void;
+
+    onKeyDown(keyCode: number): void;
+
+    focus(): void;
+
+    blur(): void;
+    
+    selectAll(): void;
+
+    setValue(v: string): void;
+
+    getValue(): string;
+
+    isEditing(): boolean;
+
+    getLastValidValue(): string;
+
+    getLastChangedValue(): string;
 }
