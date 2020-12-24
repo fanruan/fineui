4696090f5c9f2283815405ece69641eaebbd10ab
data <dep@finereport.com>
2020-12-24 19:52:25 +0800

    auto upgrade version to 2.0.20201224195215

---
+++ dist/lib/core/platform/web/function.d.ts
@@ -0,0 +1,12 @@
+export declare type _function = {
+    isIE: () => boolean;
+    getIEVersion: () => number;
+    isEdge: () => boolean;
+    isChrome: () => boolean;
+    isFireFox: () => boolean;
+    isOpera: () => boolean;
+    isSafari: () => boolean;
+    isMac: () => boolean;
+    isWindows: () => boolean;
+    isSupportCss3: (style: any) => boolean;
+};
