f193c2ab1113c2f3b2187f0450d8f8b5cbd444c5
youki <Youki@fanruan.com>
2020-12-24 13:36:28 +0800

    KERNEL-6569 refactor: 去除ClassConstructor

--- typescript/base/single/button/listitem/icontextitem.ts
+++ typescript/base/single/button/listitem/icontextitem.ts
@@ -1,18 +1,4 @@
-import { BasicButton, _BasicButton } from "../button.basic";
-
-export interface _IconTextItem extends _BasicButton {
-    doRedMark(...args: any[]): void;
-
-    unRedMark(...args: any[]): void;
-
-    doHighLight(...args: any[]): void;
-
-    unHighLight(...args: any[]): void;
-}
-
-export interface _IconTextItemStatic {
-    EVENT_CHANGE: string;
-}
+import { BasicButton } from "../button.basic";
 
 export declare class IconTextItem extends BasicButton {
     static xtype: string;
