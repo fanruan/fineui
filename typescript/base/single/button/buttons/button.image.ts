01354ee8859564e7c710f374b7119c247d74be42
Jimmy.Chai <Jimmy.Chai@fanruan.com>
2021-02-20 14:30:21 +0800

    BI-81353 chore：类型

---
+++ typescript/base/single/button/buttons/button.image.ts
@@ -0,0 +1,24 @@
+import { BasicButton } from "../button.basic";
+
+export declare class ImageButton extends BasicButton {
+    static xtype: string;
+    static EVENT_CHANGE: string;
+
+    setWidth(w: number): void;
+
+    setHeight(h: number): void;
+
+    setImageWidth(w: number): void;
+
+    setImageHeight(h: number): void;
+
+    getImageWidth(): number;
+
+    getImageHeight(): number;
+
+    setSrc(src: string): void;
+
+    getSrc(): string;
+    
+    doClick(): void;
+}
