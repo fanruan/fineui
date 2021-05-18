e263b2f41f9f39558c5cb1b6592ec8b0df15fd39
Kira <Kira@fanruan.com>
2021-05-18 14:27:38 +0800

    KERNEL-7826 refactor: props声明

--- typescript/base/single/button/listitem/icontextitem.ts
+++ typescript/base/single/button/listitem/icontextitem.ts
@@ -4,6 +4,22 @@ export declare class IconTextItem extends BasicButton {
     static xtype: string;
     static EVENT_CHANGE: string;
 
+    props: {
+        direction: string;
+        logic: {
+            dynamic: boolean;
+        };
+        iconWrapperWidth?: number;
+        iconHeight?: number;
+        iconWidth?: number;
+        textHgap: number;
+        textVgap: number;
+        textLgap: number;
+        textRgap: number;
+        text: string;
+        keyword: string;
+    } & BasicButton['props']
+
     doRedMark(...args: any[]): void;
 
     unRedMark(...args: any[]): void;
