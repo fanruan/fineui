63e140ea966dff2213cccfe219d7a72bf63e9a0e
data <dep@finereport.com>
2021-05-18 11:32:40 +0800

    auto upgrade version to 2.0.20210518113234

--- dist/fineui_without_jquery_polyfill.js
+++ dist/fineui_without_jquery_polyfill.js
@@ -1,4 +1,4 @@
-/*! time: 2021-5-18 10:10:23 AM */
+/*! time: 2021-5-18 11:30:14 AM */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
--- dist/lib/base/single/button/button.node.d.ts
+++ dist/lib/base/single/button/button.node.d.ts
@@ -1,5 +1,8 @@
 import { BasicButton } from "./button.basic";
 export declare class NodeButton extends BasicButton {
+    props: {
+        open?: boolean;
+    } & BasicButton['props'];
     isOpened(): boolean;
     setOpened(b: boolean): void;
     triggerCollapse(): void;
