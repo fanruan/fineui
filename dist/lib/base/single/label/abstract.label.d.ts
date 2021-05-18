b3c273b9cc93b4d5b7579f386eb6f49e6e824c17
data <dep@finereport.com>
2021-05-18 10:11:41 +0800

    auto upgrade version to 2.0.20210518101126

--- dist/lib/base/single/label/abstract.label.d.ts
+++ dist/lib/base/single/label/abstract.label.d.ts
@@ -1,6 +1,14 @@
 import { Single } from "../single";
 export declare class AbstractLabel extends Single {
     static xtype: string;
+    props: {
+        textAlign: "left" | "center" | "right";
+        whiteSpace: "nowrap" | "normal";
+        textWidth: number | null;
+        textHeight: number | null;
+        highLight: boolean;
+        handler: Function | null;
+    } & Single['props'];
     doRedMark(...args: any[]): void;
     unRedMark(...args: any[]): void;
     doHighLight(...args: any[]): void;
