0967002bcfc5a31f2d2660d8383fecc57edb9b98
data <dep@finereport.com>
2021-05-27 19:21:30 +0800

    auto upgrade version to 2.0.20210527192122

--- dist/fineui.js
+++ dist/fineui.js
@@ -1,4 +1,4 @@
-/*! time: 2021-5-27 4:30:23 PM */
+/*! time: 2021-5-27 7:20:19 PM */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
