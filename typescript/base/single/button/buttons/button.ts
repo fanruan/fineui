ab689b393b6d00c8fa5feafe008903a1229404c6
Kobi <Kobi@fanruan.com>
2021-06-03 10:55:26 +0800

    KERNEL-7828 根据comment优化

--- typescript/base/single/button/buttons/button.ts
+++ typescript/base/single/button/buttons/button.ts
@@ -14,8 +14,6 @@ export declare class Button extends BasicButton {
         block?: boolean; // 是否块状显示，即不显示边框，没有最小宽度的限制
         clear?: boolean; // 是否去掉边框和背景
         ghost?: boolean; // 是否幽灵显示, 即正常状态无背景
-        highLight: boolean,
-        handler: Function | null,
     } & AbstractLabel['props'] & IconLabel['props'] & BasicButton['props'];
 
     text: Label;
