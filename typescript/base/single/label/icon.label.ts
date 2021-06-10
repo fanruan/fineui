880261224375d38ff0b477f424a5db3d1818a72e
Kira <Kira@fanruan.com>
2021-06-10 09:58:23 +0800

    KERNEL-7826 refactor: 补充props声明

--- typescript/base/single/label/icon.label.ts
+++ typescript/base/single/label/icon.label.ts
@@ -2,4 +2,9 @@ import { Single } from '../single';
 
 export declare class IconLabel extends Single {
     static xtype: string;
+
+    props: {
+        iconWidth: number;
+        iconHeight: number;
+    } & Single['props'];
 }
