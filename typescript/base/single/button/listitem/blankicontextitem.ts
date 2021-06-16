943ff0661baddfc72bb20f747c4221941aef7e3e
Kobi <Kobi@fanruan.com>
2021-06-16 18:38:40 +0800

    KERNEL-7828 补充声明

--- typescript/base/single/button/listitem/blankicontextitem.ts
+++ typescript/base/single/button/listitem/blankicontextitem.ts
@@ -4,6 +4,18 @@ export declare class BlankIconTextItem extends BasicButton {
     static xtype: string;
     static EVENT_CHANGE: string;
 
+    props: {
+        blankWidth?: number;
+        iconHeight?: number | null;
+        iconWidth?: number | null;
+        textHgap?: number;
+        textVgap?: number;
+        textLgap?: number;
+        textRgap?: number;
+        text?: string;
+        keyword?: string;
+    } & BasicButton['props'];
+
     doClick(): void;
 
     setValue(): void;
