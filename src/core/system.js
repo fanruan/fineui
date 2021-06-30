b107f1d4a445adebd8905251971d8bb93a67d640
windy <1374721899@qq.com>
2021-06-30 16:51:20 +0800

    提供provider

--- src/core/system.js
+++ src/core/system.js
@@ -3,23 +3,42 @@
  * @version 2.0
  * Created by windy on 2021/6/30
  */
-_.extend(BI, {
-    switchSystemParam: function(opt) {
-        BI.config('bi.constant.system', function (ob) {
-            return BI.deepExtend(ob, opt);
-        });
-    },
-});
-
 // 系统参数常量
-BI.constant('bi.constant.system', {
-    size: { // 尺寸
-        TOOL_BAR_HEIGHT: 24,
-        LIST_ITEM_HEIGHT: 24,
-        TRIGGER_HEIGHT: 24,
-    },
-});
+!(function () {
+    var system = {
+        SIZE: { // 尺寸
+            TOOL_BAR_HEIGHT: 24,
+            LIST_ITEM_HEIGHT: 24,
+            TRIGGER_HEIGHT: 24,
+        },
+    };
+
+    var provider = function () {
+        this.inject = function (type, config) {
+            BI.deepExtend(system[type], config);
+        };
+
+        this.$get = function () {
+            return BI.inherit(BI.OB, {
+
+                getConfig: function (type) {
+                    return system[type];
+                },
+            });
+        };
+    };
+
+    BI.provider("bi.provider.system", provider);
+})();
 
 BI.prepares.push(function () {
-    BI.SIZE_CONSANTS = BI.Constants.getConstant('bi.constant.system').size;
+    BI.SIZE_CONSANTS = BI.Providers.getProvider('bi.provider.system').getConfig('SIZE');
+});
+
+BI.config('bi.provider.system', function (provider) {
+    provider.inject('SIZE', {
+        TOOL_BAR_HEIGHT: 30,
+        LIST_ITEM_HEIGHT: 30,
+        TRIGGER_HEIGHT: 30,
+    })
 });
