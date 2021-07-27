6d8da3d692d55760256b3c45b2900f1aaa10d5f0
data <dep@finereport.com>
2021-07-27 10:11:45 +0800

    auto upgrade version to 2.0.20210727101138

--- dist/utils.js
+++ dist/utils.js
@@ -1,4 +1,4 @@
-/*! time: 2021-7-27 9:01:00 */
+/*! time: 2021-7-27 10:10:30 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -5374,8 +5374,11 @@ if (!_global.BI) {
         }
         var key;
         for (var k in moduleInjectionMap) {
-            if(cls[k]){
+            if (cls[k]) {
                 for (key in cls[k]) {
+                    if (!moduleInjectionMap[k]) {
+                        continue;
+                    }
                     if (!moduleInjectionMap[k][key]) {
                         moduleInjectionMap[k][key] = [];
                     }
@@ -5464,24 +5467,25 @@ if (!_global.BI) {
                     var conf = queue[i];
                     var version = conf.opt.version;
                     var fn = conf.fn;
-                    if(modules && version) {
+                    if (modules && version) {
                         var findVersion = false;
                         for (var j = 0; j < modules.length; j++) {
                             var module = modules[i];
                             if (module && dependencies[module.moduleId] && module.version === version) {
-                                var minVersion = dependencies[module.moduleId].minVersion, maxVersion = dependencies[module.moduleId].maxVersion;
-                                if (minVersion && (moduleInjection[module.moduleId].version || version) < minVersion){
+                                var minVersion = dependencies[module.moduleId].minVersion,
+                                    maxVersion = dependencies[module.moduleId].maxVersion;
+                                if (minVersion && (moduleInjection[module.moduleId].version || version) < minVersion) {
                                     findVersion = true;
                                     break;
                                 }
-                                if(maxVersion && (moduleInjection[module.moduleId].version || version) > maxVersion){
+                                if (maxVersion && (moduleInjection[module.moduleId].version || version) > maxVersion) {
                                     findVersion = true;
                                     break;
                                 }
                             }
                         }
-                        if(findVersion === true){
-                            _global.console && console.error("module: [" + type + "] 版本: [" + module.version + "] 已过期");
+                        if (findVersion === true) {
+                            _global.console && console.error("moduleId: [" + module.moduleId + "] 服务: [" + type + "] 版本: [" + module.version + "] 已过期", modelInjection[module.moduleId]);
                             continue;
                         }
                     }
