f027ac1051dd75169ead064ff424f729083268d9
guy <guy@finereport.com>
2021-08-20 18:29:44 +0800

    整理代码

--- src/core/wrapper/layout.js
+++ src/core/wrapper/layout.js
@@ -441,10 +441,10 @@ BI.Layout = BI.inherit(BI.Widget, {
             } else {
                 var sameOldVnode = findOldVnode(oldCh, newStartVnode, oldStartIdx, oldEndIdx);
                 if (BI.isNull(sameOldVnode)) {  //  不存在就把新的放到左边
+                    delete self._children[self._getChildName(newStartIdx)];
                     var node = addNode(newStartVnode, newStartIdx);
                     insertBefore(node, oldStartVnode);
-                    newStartVnode = newCh[++newStartIdx];
-                } else {   //  如果新节点在就旧节点区间中存在就复用一下
+                } else {   //  如果新节点在旧节点区间中存在就复用一下
                     BI.each(oldCh, function (index, child) {
                         if (child && sameVnode(child, newStartVnode)) {
                             updated = self.patchItem(sameOldVnode, newStartVnode, index, index) || updated;
@@ -453,8 +453,8 @@ BI.Layout = BI.inherit(BI.Widget, {
                             insertBefore(sameOldVnode, oldStartVnode);
                         }
                     });
-                    newStartVnode = newCh[++newStartIdx];
                 }
+                newStartVnode = newCh[++newStartIdx];
             }
         }
         if (oldStartIdx > oldEndIdx) {
@@ -486,7 +486,7 @@ BI.Layout = BI.inherit(BI.Widget, {
         function addNode (vnode, index) {
             var opt = self._getOptions(vnode);
             var key = opt.key == null ? index : opt.key;
-            return children[key] = self._addElement(key, vnode);
+            return children[key] = self._addElement(index, vnode);
         }
 
         function addVnodes (before, vnodes, startIdx, endIdx) {
@@ -502,7 +502,7 @@ BI.Layout = BI.inherit(BI.Widget, {
                 if (BI.isNotNull(ch)) {
                     var node = self._getOptions(ch);
                     var key = node.key == null ? startIdx : node.key;
-                    delete self._children[self._getChildName(key)];
+                    delete self._children[self._getChildName(startIdx)];
                     children[key]._destroy();
                 }
             }
