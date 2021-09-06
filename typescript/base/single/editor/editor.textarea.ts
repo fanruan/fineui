2a102afb7ecdf180539eb6ab48c59592a07737e9
Kobi <Kobi@fanruan.com>
2021-09-06 10:44:30 +0800

    KERNEL-7828 补充声明

--- typescript/base/single/editor/editor.textarea.ts
+++ typescript/base/single/editor/editor.textarea.ts
@@ -5,6 +5,21 @@ export declare class TextAreaEditor extends Single {
     static EVENT_CHANGE: string;
     static EVENT_FOCUS: string;
     static EVENT_BLUR: string;
+    static EVENT_CONFIRM: string;
+    static EVENT_EMPTY: string;
+    static EVENT_KEY_DOWN: string;
+
+    props: {
+        value?: string;
+        errorText?: string | ((v: string) => string)
+        adjustYOffset?: number;
+        adjustXOffset?: number;
+        offsetStyle?: string;
+        validationChecker?: Function;
+        scrolly?: boolean;
+        style?: any;
+        watermark?: string;
+    } & Single['props'];
 
     focus(): void;
 
