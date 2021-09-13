7dbaa50dbe7b87b9c8cfa8aee3640beecffd9428
data <dep@finereport.com>
2021-09-13 18:22:28 +0800

    auto upgrade version to 2.0.20210913182210

--- dist/fineui.proxy.js
+++ dist/fineui.proxy.js
@@ -1,4 +1,4 @@
-/*! time: 2021-9-12 15:31:57 */
+/*! time: 2021-9-13 18:20:55 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
