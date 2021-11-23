d99d2c3f7a2a5251b06fcc78fd31a98b5306dfc6
imp <imp@fanruan.com>
2021-11-23 14:51:36 +0800

    无jira任务 类型

--- typescript/base/single/button/button.basic.ts
+++ typescript/base/single/button/button.basic.ts
@@ -39,7 +39,7 @@ export declare class BasicButton extends Single {
         handler?: Function;
         bubble?: Function | null | string;
         text?: string;
-        el: Obj;
+        el?: Obj;
     } & Single['props'];
 
     _createShadow(): void;
