e482cfbae67227d30f11ff691a7382f31c539830
iapyang <yklorse@gmail.com>
2021-12-07 11:13:32 +0800

    chore: 修改描述

--- typescript/base/single/text.ts
+++ typescript/base/single/text.ts
@@ -3,6 +3,10 @@ import { Single } from "./single";
 export declare class Text extends Single {
     static xtype: string;
 
+    static addTextFormatter: (fn: (str: string) => string) => void;
+
+    static formatText: (str: string) => string;
+
     props: {
         text?: string;
         textAlign?: "left" | "center" | "right",
