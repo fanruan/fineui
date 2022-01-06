25bae6661cacbce21db02fe6cdac73ac96755ff7
windy <1374721899@qq.com>
2022-01-06 17:23:58 +0800

    KERNEL-9882 完成checkbox radio switch整理

--- src/case/button/switch.js
+++ src/case/button/switch.js
@@ -5,15 +5,16 @@ BI.Switch = BI.inherit(BI.BasicButton, {
 
     props: {
         extraCls: "bi-switch",
-        height: 22,
+        height: 20,
         width: 44,
         logic: {
             dynamic: false
-        }
+        },
+        showTip: false
     },
 
     render: function () {
-        var self = this;
+        var self = this, o = this.options;
         return {
             type: "bi.absolute",
             ref: function () {
@@ -22,20 +23,42 @@ BI.Switch = BI.inherit(BI.BasicButton, {
             items: [{
                 el: {
                     type: "bi.text_button",
-                    cls: "circle-button bi-card"
+                    cls: "circle-button"
                 },
-                width: 18,
-                height: 18,
+                width: 12,
+                height: 12,
+                top: 4,
+                left: this.options.selected ? 28 : 4
+            }, {
+                type: "bi.label",
+                text: BI.i18nText("BI-Basic_Open"),
+                cls: "content-tip",
+                left: 8,
                 top: 2,
-                left: this.options.selected ? 24 : 2
+                invisible: !o.showTip,
+                ref: function (ref) {
+                    self.openTip = ref;
+                }
+            }, {
+                type: "bi.label",
+                text: BI.i18nText("BI-Basic_Close"),
+                cls: "content-tip",
+                right: 8,
+                top: 2,
+                invisible: !o.showTip,
+                ref: function (ref) {
+                    self.closeTip = ref;
+                }
             }]
         };
     },
 
     setSelected: function (v) {
         BI.Switch.superclass.setSelected.apply(this, arguments);
-        this.layout.attr("items")[0].left = v ? 24 : 2;
+        this.layout.attr("items")[0].left = v ? 28 : 4;
         this.layout.resize();
+        this.options.showTip && this.openTip.setVisible(v);
+        this.options.showTip && this.closeTip.setVisible(!v);
     },
 
     doClick: function () {
