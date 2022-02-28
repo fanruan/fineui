5bad2781e59e8b3d1007a8b51da76d7007daddf7
Guyi <Guyi@fanruan.com>
2022-02-28 10:56:35 +0800

    无jira任务，补充描述

--- typescript/widget/downlist/item.downlistgroup.ts
+++ typescript/widget/downlist/item.downlistgroup.ts
@@ -4,6 +4,11 @@ export declare class DownListGroupItem extends BasicButton {
     static xtype: string;
     static EVENT_CHANGE: string;
 
+    props: {
+        iconCls1: string;
+        iconCls2: string;
+    } & BasicButton['props'];
+
     doRedMark(...args: any[]): void;
 
     unRedMark(...args: any[]): void;
