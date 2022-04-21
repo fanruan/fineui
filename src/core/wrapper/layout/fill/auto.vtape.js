624c64e8aebb8dc791ecf41bed2389114aee29af
zsmj <zsmj1994@gmail.com>
2022-04-21 11:10:09 +0800

    Revert "无JIRA  去掉无意义的scrolly"
    This reverts commit eadbe4ec2deee6b1f7cb5da2b74bce3ea02a2c3d.

--- src/core/wrapper/layout/fill/auto.vtape.js
+++ src/core/wrapper/layout/fill/auto.vtape.js
@@ -1,7 +1,7 @@
 BI.AutoVerticalTapeLayout = BI.inherit(BI.Layout, {
     props: function () {
         return BI.extend(BI.AutoVerticalTapeLayout.superclass.props.apply(this, arguments), {
-            baseCls: "bi-auto-vtape",
+            baseCls: "bi-auto-htape",
             horizontalAlign: BI.HorizontalAlign.Stretch,
             verticalAlign: BI.VerticalAlign.Stretch,
             hgap: 0,
